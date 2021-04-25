f287220e2c0b7a25bac9f48687a9af2db0bc0c6e
Turan Ates <jamesdusky@dustytavern.com>
2021-04-25 19:42:11 -0400

    Test

--- organization/index.php
+++ organization/index.php
@@ -6,61 +6,40 @@
 
   $user = "Stesig";
   $rank = 5;
-
-  $json = file_get_contents("https://api.starcitizen-api.com/".$key."/v1/auto/user/".$user);
-
+  $json = file_get_contents("https://api.starcitizen-api.com/".$key."/v1/auto/organization/EVILORG");
   $xmlResult = json_decode($json, true);
-  $updateOrgSID = $xmlResult['data']['organization']['sid'];
 
-  $sql = "SELECT sid FROM organizations WHERE sid = '$updateOrgSID'";
-  $result = mysqli_query($link, $sql);
-  $row = mysqli_fetch_assoc($result);
+  $memberCount = $xmlResult['data']['members'];
+  $updateOrgName = $xmlResult['data']['name'];
+  $updateOrgLogo = $xmlResult['data']['logo'];
 
-  if($row['sid'] !== $updateOrgSID){
-    $json = file_get_contents("https://api.starcitizen-api.com/".$key."/v1/auto/organization/".$updateOrgSID);
+  $grossPages = ceil($memberCount/32);
+
+  $json = file_get_contents("https://api.starcitizen-api.com/".$key."/v1/live/organization_members/".$updateOrgSID."?rank=1");
+  $xmlResult = json_decode($json, true);
+
+  $updateOrgLeaders = array();
+  $updateOrgMembers = array();
+  $x=0;
+  foreach ($xmlResult['data'] as $leader => $l) {
+    $updateOrgLeaders[$x] = [
+        "handle"=>$l['handle'],
+        "rank"=>$l['stars']
+      ];
+      $x++;
+  }
+
+
+  $x=0;
+  for($i=0;$i<$grossPages;$i++){
+    $json = file_get_contents("https://api.starcitizen-api.com/".$key."/v1/live/organization_members/".$updateOrgSID."?page=".$i);
     $xmlResult = json_decode($json, true);
-
-    $memberCount = $xmlResult['data']['members'];
-    $updateOrgName = $xmlResult['data']['name'];
-    $updateOrgLogo = $xmlResult['data']['logo'];
-
-    $grossPages = ceil($memberCount/32);
-
-    $json = file_get_contents("https://api.starcitizen-api.com/".$key."/v1/live/organization_members/".$updateOrgSID."?rank=1");
-    $xmlResult = json_decode($json, true);
-
-    $updateOrgLeaders = array();
-    $updateOrgMembers = array();
-    $x=0;
-    foreach ($xmlResult['data'] as $leader => $l) {
-      $updateOrgLeaders[$x] = [
-          "handle"=>$l['handle'],
-          "rank"=>$l['stars']
-        ];
+    foreach ($xmlResult['data'] as $member => $m){
+      if($m['stars'] < $rank){
+        $updateOrgMembers[$x] = [$m['handle']];
         $x++;
-    }
-
-
-    $x=0;
-    for($i=0;$i<$grossPages;$i++){
-      $json = file_get_contents("https://api.starcitizen-api.com/".$key."/v1/live/organization_members/".$updateOrgSID."?page=".$i);
-      $xmlResult = json_decode($json, true);
-      foreach ($xmlResult['data'] as $member => $m){
-        if($m['stars'] < $rank){
-          $updateOrgMembers[$x] = [$m['handle']];
-          $x++;
-        }
       }
     }
-    echo "!search ".join(" ", $updateOrgMembers);
-    /*
-    $supLeaders = new UniqueValues($updateOrgLeaders,"handle");
-    $supMembers = new UniqueValues($updateOrgMembers,"handle");
-    $leaderInjext = stripslashes(json_encode($supLeaders->getFiltered(), JSON_FORCE_OBJECT));
-    $memberInjext = stripslashes(json_encode($supMembers->getFiltered(), JSON_FORCE_OBJECT));
-    $sql = "INSERT INTO organizations (name, sid, logo, leaders, members) VALUES ('$updateOrgName', '$updateOrgSID', '$updateOrgLogo', '$leaderInjext', '$memberInjext')";
-    mysqli_query($link, $sql);
-    mysqli_close($link);
-    */
   }
+  echo "!search ".join(" ", $updateOrgMembers);
 ?>
