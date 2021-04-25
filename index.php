920be23da2ed3ef0c814fefa4259525c88e9cb3b
Turan Ates <jamesdusky@dustytavern.com>
2021-04-24 22:54:31 -0400

    CSS

--- index.php
+++ index.php
@@ -111,7 +111,7 @@
         <div id="login_status" class="login_menu">
           <a><img id="mtLogo" src="android-chrome-512x512.png" class="mtLogo"></a>
           <a id="mtTitle" class="mtTitle">MOBITRACKER</a>
-          <a class="verify_btn">Login</a>
+          <a class="verify_btn lmBtn">Login</a>
           <a class="verify_btn">Sign Up</a>
           <div id="navBtnContainer" class="navBtnContainer">
             <div class="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "loggedIn";}else{echo "loggedOut";} ?>"></div>
