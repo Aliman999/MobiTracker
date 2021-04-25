2f2b53c7ca288978d2a8028480f4a8aa2a450374
Turan Ates <jamesdusky@dustytavern.com>
2021-04-25 19:39:36 -0400

    Org Supply

--- organization/config.php
+++ organization/config.php
@@ -1,12 +1,37 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'login');
-define('DB_PASSWORD', 'Ninjaman');
-define('DB_NAME', 'users');
+if (isset($_SESSION['token'])) {
+  define('DB_SERVER', 'localhost');
+  define('DB_USERNAME', 'login');
+  define('DB_PASSWORD', 'Ninjaman');
+  define('DB_NAME', 'users');
 
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+
+  function selectKey(){
+    $apiKeys = array(
+    "eb275472840a55ff74bcca0ba7baced0",
+    "c032ae79fa8a389e5b6e2f17a191643e",
+    "608c3b491aed563faca4bfef14f70c1c",
+    "7d3192f880dee8c748ef4e02ee39b447",
+    "c13b1badf9ccd433c90b4160c7664107"
+    );
+
+    return $apiKeys[array_rand($apiKeys, 1)];
+  }
+
+  $key = selectKey();
+
+  $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+  if($link === false){
+      die("ERROR: Could not connect. " . mysqli_connect_error());
+  }
+}else{
+  $var = preg_split("#/#", $_SERVER['REQUEST_URI']);
+  foreach ($var as $var => $v) {
+    if(!empty($v) && $v != "beta"){
+      header("Location: ../login?ref=".$v);
+    }
+  }
 }
 ?>
--- organization/index.php
+++ organization/index.php
@@ -1,82 +1,66 @@
 <?php
-  // Initialize the session
-  session_start();
-  if (empty($_SESSION['token']) || empty($_SESSION['loggedin'])) {
-    header("location: ../");
+  require_once "config.php";
+  if(!$link ) {
+    die('Could not connect: ' . mysqli_error());
   }
-  require_once "vadw.php";
-?>
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>MobiTracker - Org Management</title>
-    <link href="https://fonts.googleapis.com/css2?family=Exo:wght@300;400;500;600&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="locale.css">
-    <link rel="apple-touch-icon" sizes="180x180" href="../apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="../favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="../favicon-16x16.png">
-    <link rel="manifest" href="../site.webmanifest">
-    <link rel="mask-icon" href="../safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#253139">
-    <?php
-    echo "<meta name='token' content=".$_SESSION['token'].">";
-    ?>
-  </head>
-  <body>
-    <div id="login_status" class="login_menu">
-      <?php
-      if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-        if($_SESSION['verified'] === 1){
-          echo '<img class="verified" src="../src/verified.png"></a>';
-        }
-        echo "<p class='loginName' onclick='clickUser(this.innerText)'>".$_SESSION["username"]." </p>";
-        echo '<a href="../" class="verify_btn home_btn">Home</a>';
-        echo '<div class="loggedinDiv"></div>';
-        echo '<a href="../signout" class="logout_btn">Sign out</a>';
-      }else{
-        echo '<a href=../login class="login_btn">Sign in</a><div class="loginDiv"></div> <a href=../register class="login_btn"> Sign up</a><a href="../" class="verify_btn home_btn">Home</a>';
-      }
-      ?>
-    </div>
-    <div class="web-title">
-      <p class="web-name">MobiTracker Organization Manager</p>
-    </div>
-    <p class="home" id="moto">This page is for organization leaders with rank 5.
-      If you'd like to expand this functionality to other ranks please DM <a class="linkme" href="https://www.mobitracker.co?search=JamesDusky">JamesDusky</a><br/>Discord: Aliman#5518</p>
-    <div id="orgInfo">
-      <span class="highlight-red">If this is your first time and your org is large this may take some time...</span>
-      Loading...
-    </div>
-    <div class="container container-section">
-      <div class="collapseB">
-        <p>Leaders</p>
-        <p class="collapse"></p>
-      </div>
-      <section class="container-comments">
-        <div id="bossContainer" class="bossContainer bC">
-        </div>
-      </section>
-    </div>
 
-    <div class="container container-header">
-      <div class="collapseB">
-        <p>Members</p>
-        <p class="collapse"></p>
-      </div>
-      <div id="inputContainer" class="memSearch">
-        <input id="userInput" type="text" class="userInput" autofocus="" onclick="this.select()" value="" placeholder="Enter a Star Citizen's Handle">
-      </div>
-      <div id="pageContainer" class="pageContainer">
-      </div>
-      <header class="playercard">
-      </header>
-      <div id="pageContainer" class="pageContainer">
-      </div>
-    </div>
-    <script type="text/javascript" src="main.js" async></script>
-  </body>
-</html>
+  $user = "Stesig";
+  $rank = 5;
+
+  $json = file_get_contents("https://api.starcitizen-api.com/".$key."/v1/auto/user/".$user);
+
+  $xmlResult = json_decode($json, true);
+  $updateOrgSID = $xmlResult['data']['organization']['sid'];
+
+  $sql = "SELECT sid FROM organizations WHERE sid = '$updateOrgSID'";
+  $result = mysqli_query($link, $sql);
+  $row = mysqli_fetch_assoc($result);
+
+  if($row['sid'] !== $updateOrgSID){
+    $json = file_get_contents("https://api.starcitizen-api.com/".$key."/v1/auto/organization/".$updateOrgSID);
+    $xmlResult = json_decode($json, true);
+
+    $memberCount = $xmlResult['data']['members'];
+    $updateOrgName = $xmlResult['data']['name'];
+    $updateOrgLogo = $xmlResult['data']['logo'];
+
+    $grossPages = ceil($memberCount/32);
+
+    $json = file_get_contents("https://api.starcitizen-api.com/".$key."/v1/live/organization_members/".$updateOrgSID."?rank=1");
+    $xmlResult = json_decode($json, true);
+
+    $updateOrgLeaders = array();
+    $updateOrgMembers = array();
+    $x=0;
+    foreach ($xmlResult['data'] as $leader => $l) {
+      $updateOrgLeaders[$x] = [
+          "handle"=>$l['handle'],
+          "rank"=>$l['stars']
+        ];
+        $x++;
+    }
+
+
+    $x=0;
+    for($i=0;$i<$grossPages;$i++){
+      $json = file_get_contents("https://api.starcitizen-api.com/".$key."/v1/live/organization_members/".$updateOrgSID."?page=".$i);
+      $xmlResult = json_decode($json, true);
+      foreach ($xmlResult['data'] as $member => $m){
+        if($m['stars'] < $rank){
+          $updateOrgMembers[$x] = [$m['handle']];
+          $x++;
+        }
+      }
+    }
+    echo "!search ".join(" ", $updateOrgMembers);
+    /*
+    $supLeaders = new UniqueValues($updateOrgLeaders,"handle");
+    $supMembers = new UniqueValues($updateOrgMembers,"handle");
+    $leaderInjext = stripslashes(json_encode($supLeaders->getFiltered(), JSON_FORCE_OBJECT));
+    $memberInjext = stripslashes(json_encode($supMembers->getFiltered(), JSON_FORCE_OBJECT));
+    $sql = "INSERT INTO organizations (name, sid, logo, leaders, members) VALUES ('$updateOrgName', '$updateOrgSID', '$updateOrgLogo', '$leaderInjext', '$memberInjext')";
+    mysqli_query($link, $sql);
+    mysqli_close($link);
+    */
+  }
+?>
