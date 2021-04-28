5b6f8d6508a0056c13a8276642eec517c3b90e37
Turan Ates <jamesdusky@dustytavern.com>
2021-04-28 12:42:28 -0400

    debug

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
     if($_SESSION['banned'] == 1){
       header("location: signout");
     }
+
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 ?>
 <!DOCTYPE html>
 <html>
