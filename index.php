a9af5712be5a9111ed5ddc09a716d8888881991d
Turan Ates <jamesdusky@dustytavern.com>
2021-04-29 13:22:33 -0400

    test

--- index.php
+++ index.php
@@ -104,8 +104,8 @@
         <div id="login_status" class="login_menu">
           <a><img id="mtLogo" src="android-chrome-512x512.png" class="mtLogo"></a>
           <a id="mtTitle" class="mtTitle">MOBITRACKER</a>
-          <?php if($_SESSION["loggedin"] != true){echo "<a id='lmLBtn' class='verify_btn lmBtn'>Login</a>";} ?>
-          <?php if($_SESSION["loggedin"] != true){echo "<a id='lmSBtn' class='verify_btn'>Sign Up</a>";} ?>
+          <?php if(!isset($_SESSION["loggedin"])){echo "<a id='lmLBtn' class='verify_btn lmBtn'>Login</a>";} ?>
+          <?php if(!isset($_SESSION["loggedin"])){echo "<a id='lmSBtn' class='verify_btn'>Sign Up</a>";} ?>
           <div id="navBtnContainer" class="navBtnContainer <?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "lmBtn";} ?>">
             <div class="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "loggedIn";}else{echo "loggedOut";} ?>"></div>
             <div class="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "loggedIn";}else{echo "loggedOut";} ?>"></div>
