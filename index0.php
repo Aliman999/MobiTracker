6935435622dfa0a5bf0d424270a91294e67f7fa2
Turan Ates <jamesdusky@dustytavern.com>
2021-04-30 17:23:42 -0400

    Test

--- index0.php
+++ index0.php
@@ -16,7 +16,6 @@ function selectKey(){
   for($i = 0, $i < length($apiKeys), $i++){
     echo min(array_column($apiKeys, 1));
   }
-  return $apiKeys[array_rand($apiKeys, 1)];
 }
 
 $key = selectKey();
