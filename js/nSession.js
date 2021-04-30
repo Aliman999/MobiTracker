6a9190ffa8c71943268805fc10ebbb3c6eeb91df
Turan Ates <jamesdusky@dustytavern.com>
2021-04-30 15:21:45 -0400

    Testing session js

--- js/nSession.js
+++ js/nSession.js
@@ -1,4 +1,6 @@
 var openBtn = document.getElementById("navBtnContainer");
+var openLoginBtn = document.getElementById("lmLBtn");
+var openSignUpBtn = document.getElementById("lmSBtn");
 var closeBtn = document.getElementById("closeNavBtn");
 var eleCount = document.getElementsByClassName("navEle");
 var navMenu = document.getElementById("navMenu");
@@ -6,36 +8,6 @@ var iContainer = document.getElementById("iContainer");
 var query = new XMLHttpRequest();
 iContainer.sibling = document.getElementById("sPContainer");
 openBtn.active = false;
-
-
-var getUser = new XMLHttpRequest();
-var session,
-    sessionUser,
-    comcount,
-    search,
-    limited,
-    verified,
-    flagged,
-    faction;
-getUser.open("GET", "https://mobitracker.co/src/user.php");
-getUser.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-getUser.setRequestHeader(tokenHeader.name,tokenHeader.content);
-getUser.responseType = "json";
-getUser.async = false;
-getUser.send();
-getUser.onload = function(){
-  var userResponse = getUser.response;
-  session = userResponse["session"];
-  sessionUser = userResponse["sessionUser"];
-  comcount = userResponse["comcount"];
-  search = userResponse["search"];
-  limited = userResponse["limited"];
-  verified = userResponse["verified"];
-  flagged = userResponse["flagged"];
-  faction = userResponse["faction"];
-  avatar = userResponse["avatar"];
-}
-
 
 openBtn.onclick = function(){
   if(this.active){
