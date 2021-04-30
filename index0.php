ec33507b8d7c1430f6dd8ac1895688b9670dc57d
Turan Ates <jamesdusky@dustytavern.com>
2021-04-30 18:06:50 -0400

    test

--- index0.php
+++ index0.php
@@ -5,17 +5,32 @@ error_reporting(E_ALL);
 $_SESSION['token'] = "SET";
 require_once "src/config.php";
 
-$sql = "SELECT apiKey FROM apiKeys where note like '%index0.php%' GROUP BY apiKey, count ORDER BY count desc LIMIT 1;";
-$result = mysqli_query($link, $sql);
-$key = mysqli_fetch_assoc($result);
-$key = $key['apiKey'];
-var_dump($key);
+$key = $count = $id = "";
 
+function getKey(){
+  global $id, $key, $count;
+  $sql = "SELECT id, apiKey, count FROM apiKeys WHERE note like '%index0.php%' GROUP BY apiKey, count ORDER BY count desc LIMIT 1;";
+  $result = mysqli_query($link, $sql);
+  $key = mysqli_fetch_assoc($result);
+  $id = $key['id'];
+  $count = $key['count'];
+  $key = $key['apiKey'];
+}
 
+function setKey(){
+  global $id, $key, $count;
+  if($key != ""){
+    $count = $count-1;
+    $sql = "UPDATE apiKeys SET count = $count WHERE id = $id";
+    mysqli_query($link, $sql);
+  }else{
+    return "No Key";
+  }
+}
 
-$updateOrgSID = "EVILORG";
-/*
-$json = file_get_contents("https://api.starcitizen-api.com/".$key."/v1/auto/organization/".$updateOrgSID);
+$updateOrgSID = "ASTROLLC";
+
+$json = file_get_contents("https://api.starcitizen-api.com/".getKey()."/v1/auto/organization/".$updateOrgSID);
 $xmlResult = json_decode($json, true);
 
 $memberCount = $xmlResult['data']['members'];
@@ -25,10 +40,11 @@ $grossPages = ceil($memberCount/32);
 $x=0;
 $orgMembers = array();
 for($i=0;$i<$grossPages;$i++){
-  $json = file_get_contents("https://api.starcitizen-api.com/".$key."/v1/live/organization_members/".$updateOrgSID."?page=".$i);
+  $json = file_get_contents("https://api.starcitizen-api.com/".getKey()."/v1/live/organization_members/".$updateOrgSID."?page=".$i);
   $xmlResult = json_decode($json, true);
   foreach ($xmlResult['data'] as $member => $m){
     $orgMembers[$x] = $m['handle'];
+    setKey();
     $x++;
   }
 }
