f13657380b00a87d7e8bfad9afeed792b8c32b97
Turan Ates <jamesdusky@dustytavern.com>
2021-05-01 00:41:36 -0400

    fixing css

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
               <p class="loginName">JamesDusky</p>
             </div>
             <div class="settingsContainer">
-              <a class="rButton highlight-green" href="">Settings</a>
+              <a class="rButton highlight" href="">Settings</a>
             </div>
           </div>
           <div class="miniRep">
