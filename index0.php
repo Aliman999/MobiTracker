3c152043c3f7ee8d42a3f1769ace6fff9e8520c6
Turan Ates <jamesdusky@dustytavern.com>
2021-05-02 15:25:19 -0400

    Scanning new Org

--- index0.php
+++ index0.php
@@ -29,7 +29,7 @@ function setKey(){
   }
 }
 
-$updateOrgSID = "UETG";
+$updateOrgSID = "JIANGJUNFU";
 
 $json = file_get_contents("https://api.starcitizen-api.com/".getKey()."/v1/auto/organization/".$updateOrgSID);
 $xmlResult = json_decode($json, true);
