7f4944495ffff83fcb4f38f6ceb76873e1f3898c
Turan Ates <jamesdusky@dustytavern.com>
2021-05-02 20:42:48 -0400

    Scanning new org

--- index0.php
+++ index0.php
@@ -20,7 +20,7 @@ function getKey(){
   return $key;
 }
 
-$updateOrgSID = "TIEBA";
+$updateOrgSID = "CLOUDAS";
 
 $json = file_get_contents("https://api.starcitizen-api.com/".getKey()."/v1/auto/organization/".$updateOrgSID);
 $xmlResult = json_decode($json, true);
