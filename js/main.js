968f8766f35d21ad4b4478cc055f63a1bc7acefe
Turan Ates <jamesdusky@dustytavern.com>
2021-05-10 13:04:15 -0700

    Debug

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ var updateCareerXML = new XMLHttpRequest();
 
 var c = 0;
 var waitUser = setInterval(function(){
-  if(!user){
+  if(getUser){
     if(c == 3){
       clearInterval(waitUser);
     }
