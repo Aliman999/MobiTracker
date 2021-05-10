93bbd6f36fd1a0d5e4d30410464d13299da3062e
Turan Ates <jamesdusky@dustytavern.com>
2021-05-09 21:06:16 -0400

    Testing Async

--- index.php
+++ index.php
@@ -160,7 +160,7 @@
         </div>
     </div>
     <script type="text/javascript" src="js/socket.js" async></script>
-    <script type="text/javascript" src="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "js/nav.js";}else{echo "js/nSession.js";} ?>"></script>
+    <script type="text/javascript" src="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "js/nav.js";}else{echo "js/nSession.js";} ?>" async></script>
     <script type="text/javascript" src="js/main.js"></script>
     <script type="text/javascript" src="js/events.js"></script>
   </body>
