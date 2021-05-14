1825247c100db7bc1c047706c469ba343ed08839
Turan Ates <jamesdusky@dustytavern.com>
2021-05-14 08:51:28 -0400

    Debug

--- vadw.php
+++ vadw.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
+echo $_SESSION['search'];
 if(isset($_GET['search'])){
   $_SESSION['search'] = $_GET['search'];
 }else{
   $_SESSION['search'] = "";
 }
-echo "New Search - ".$_SESSION['search'];
 ?>
