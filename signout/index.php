4178767c503464c1e94b4f637db1a69025c2353b
Turan Ates <jamesdusky@dustytavern.com>
2021-05-19 22:46:39 -0400

    debug

--- signout/index.php
+++ signout/index.php
@@ -1,12 +1,16 @@
 <?php
-  session_start();
-  if(basename($_SERVER['HTTP_REFERER']) != "beta")){
-    $ref = basename($_SERVER['HTTP_REFERER']);
-  }
-  var_dump($ref);
-  //unset($_SESSION);
-  //session_destroy();
-  //session_write_close();
-  //header('Location: ../'.$ref);
-  //die;
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+if(basename($_SERVER['HTTP_REFERER']) != "beta")){
+  $ref = basename($_SERVER['HTTP_REFERER']);
+}
+var_dump($ref);
+//unset($_SESSION);
+//session_destroy();
+//session_write_close();
+//header('Location: ../'.$ref);
+//die;
 ?>
