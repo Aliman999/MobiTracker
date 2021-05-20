55654579a6fefcc5737c63167da5885ab95854cc
Turan Ates <jamesdusky@dustytavern.com>
2021-05-19 22:45:21 -0400

    Debug

--- signout/index.php
+++ signout/index.php
@@ -1,6 +1,8 @@
 <?php
   session_start();
-  $ref = basename($_SERVER['HTTP_REFERER']);
+  if(basename($_SERVER['HTTP_REFERER'] != "beta")){
+    $ref = basename($_SERVER['HTTP_REFERER']);
+  }
   var_dump($ref);
   //unset($_SESSION);
   //session_destroy();
