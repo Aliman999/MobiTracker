df7386035c94072f965fb152388a9605fd23a9e8
Turan Ates <jamesdusky@dustytavern.com>
2021-05-19 22:59:00 -0400

    Changed active nav element

--- discord/index.php
+++ discord/index.php
@@ -74,9 +74,9 @@ session_start();
         </div>
       </div>
       <div class="eleContainer">
-        <a class="navEle navActive"><img src="../src/repHub.png" class="menuIcon"/><p>Reputation Hub</p></a>
+        <a class="navEle"><img src="../src/repHub.png" class="menuIcon"/><p>Reputation Hub</p></a>
         <a href="contracts" class="navEle"><img src="../src/contracts.png" class="menuIcon"/><p>Contracts</p></a>
-        <a href="discord" class="navEle"><img src="../src/discord.png" class="menuIcon"/><p>Discord</p></a>
+        <a href="discord" class="navEle navActive"><img src="../src/discord.png" class="menuIcon"/><p>Discord</p></a>
         <a href="faq" class="navEle"><img src="../src/faq.png" class="menuIcon"/><p>FAQ</p></a>
         <a href="https://patreon.com/mobitracker" class="navEle"><img src="../src/patreon.png" class="menuIcon"/><p>Patreon</p></a>
       </div>
