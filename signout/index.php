746aedff5d973abd70746eaac7710093f28cab19
Turan Ates <jamesdusky@dustytavern.com>
2021-05-19 22:29:32 -0400

    Debug

--- signout/index.php
+++ signout/index.php
@@ -1,7 +1,6 @@
 <?php
   session_start();
-  var_dump($_SERVER['HTTP_REFERER']);
-  $ref = explode("/", $_SERVER['HTTP_REFERER']);
+  $ref = dirname($_SERVER['HTTP_REFERER']);
   var_dump($ref);
   //unset($_SESSION);
   //session_destroy();
