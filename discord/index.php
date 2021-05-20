bf31a3f38071c9276af175ff339cf4c0da66d7ed
Turan Ates <jamesdusky@dustytavern.com>
2021-05-19 21:51:04 -0400

    Changing discord page

--- discord/index.php
+++ discord/index.php
@@ -4,6 +4,139 @@ session_start();
 ?>
 
 <!DOCTYPE html>
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>MobiTracker</title>
+    <link href="https://fonts.googleapis.com/css2?family=Exo:wght@300;400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/patreon.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="https://mobitracker.co/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://mobitracker.co/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://mobitracker.co/favicon-16x16.png">
+    <link rel="manifest" href="https://mobitracker.co/site.webmanifest">
+    <link rel="mask-icon" href="https://mobitracker.co/safari-pinned-tab.svg" color="#5bbad5">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="MobiTracker.co" />
+    <meta property="og:description" content="Find who to trust when you need a hand to lead your armed escort or who to avoid letting on your ship" />
+    <meta property="og:url" content="https://mobitracker.co" />
+    <meta property="og:image" content="https://mobitracker.co/android-chrome-512x512.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#253139">
+    <?php
+    echo "<meta name='token' content=".$_SESSION['token'].">";
+    ?>
+    <?php
+    if($_SESSION["loggedin"]){
+      echo "<meta name='jwt' content=".$_SESSION['jwt'].">";
+    }
+    ?>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-177343701-1"></script>
+    <script type="text/javascript" src="js/ga.js"></script>
+  </head>
+
+  <body>
+    <div id="navMenu" class="navMenu">
+      <div class="navTitleContainer">
+        <p class="navTitle">NAVIGATE</p>
+        <a href="javascript:void(0)" class="closebtn" id="closeNavBtn"><img src="src/close.png" class="navBtn"></a>
+      </div>
+      <div class="miniPlayerDivOut">
+        <div class="miniPlayerOut">
+          <?php
+            if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+              echo "<div class='miniPlayer' id='miniPlayer'></div><div class='miniRep' id='miniRep'></div>";
+            }else{
+              echo "<div class='mpLoginContainer' id='mpLoginContainer'></div>";
+            }
+          ?>
+          <!--
+          <div class="miniPlayer">
+            <img src="src/avatars/avatar_default.jpg" class="avatar circleBorder">
+            <div class="loginNameContainer">
+              <img class="verified" src="src/verified.png">
+              <p class="loginName">JamesDusky</p>
+            </div>
+            <div class="settingsContainer">
+              <a class="rButton highlight settings" href="">Settings</a>
+            </div>
+          </div>
+          <div class="miniRep">
+            <p class="rBold">Experienced</p>
+            <p>Vouches: 54</p>
+            <p>Unique Vouches: 54</p>
+            <p>Completed Contracts: 1</p>
+          </div>
+        -->
+        </div>
+      </div>
+      <div class="eleContainer">
+        <a class="navEle navActive"><img src="src/repHub.png" class="menuIcon"/><p>Reputation Hub</p></a>
+        <a href="contracts" class="navEle"><img src="src/contracts.png" class="menuIcon"/><p>Contracts</p></a>
+        <a href="discord" class="navEle"><img src="src/discord.png" class="menuIcon"/><p>Discord</p></a>
+        <a href="faq" class="navEle"><img src="src/faq.png" class="menuIcon"/><p>FAQ</p></a>
+        <a href="https://patreon.com/mobitracker" class="navEle"><img src="src/patreon.png" class="menuIcon"/><p>Patreon</p></a>
+      </div>
+    </div>
+    <div id="navBackDrop" class="navBackDrop"></div> <!-- REQUIRED FOR NAV -->
+    <div id="canvas">
+      <div class="headerContainer">
+        <div id="login_status" class="login_menu">
+          <a><img id="mtLogo" src="android-chrome-512x512.png" class="mtLogo"></a>
+          <a id="mtTitle" class="mtTitle">MOBITRACKER</a>
+          <?php
+          if(!isset($_SESSION["loggedin"])){
+            echo "<a id='lmLBtn' class='verify_btn lmBtn'>Login</a>";
+            echo "<a id='lmSBtn' class='verify_btn'>Sign Up</a>";
+          }
+          if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+            echo "<div class='userContainer'><img class='verified' src='src/verified.png'><p class='loginName'>".$_SESSION['username']."</p></div>";
+          }
+          ?>
+          <div id="navBtnContainer" class="navBtnContainer">
+            <div class="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "loggedIn";}else{echo "loggedOut";} ?>"></div>
+            <div class="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "loggedIn";}else{echo "loggedOut";} ?>"></div>
+            <div class="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "loggedIn";}else{echo "loggedOut";} ?>"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="web-title">
+        <img class="sc-logo" src="Star-Citizen.png" alt="">
+        <p class="web-slogan">Reputation Hub</p>
+      </div>
+
+      <div class="container container-header" id="container">
+        <div id="pageContainer" class="pageContainer">
+        </div>
+        <header class="playercard">
+          <?php
+          include "auth.php";
+          ?>
+        </header>
+        <div id="pageContainer" class="pageContainer">
+        </div>
+      </div>
+
+      <div class="container container-section" id="container">
+        <div id="pageContainer" class="pageContainer">
+        </div>
+        <section class="container-comments">
+        </section>
+        <div id="pageContainer" class="pageContainer">
+        </div>
+      </div>
+
+    </div>
+    <script type="text/javascript" src="../js/socket.js"></script>
+    <script type="text/javascript" src="main.js"></script>
+    <script type="text/javascript" src="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "../js/nav.js";}else{echo "../js/nSession.js";} ?>" async></script>
+  </body>
+</html>
+<!--
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -21,6 +154,7 @@ session_start();
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#253139">
     <?php
+    /*
     echo "<meta name='token' content=".$_SESSION['token'].">";
     ?>
 </head>
