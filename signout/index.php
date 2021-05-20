0818daa7fd878a9c2f51f03466dfef3d82f08fcc
Turan Ates <jamesdusky@dustytavern.com>
2021-05-19 22:31:32 -0400

    Debug

--- signout/index.php
+++ signout/index.php
@@ -1,9 +1,9 @@
 <?php
   session_start();
   $ref = basename($_SERVER['HTTP_REFERER']);
-  unset($_SESSION);
-  session_destroy();
-  session_write_close();
-  header('Location: ../'.$ref);
-  die;
+  //unset($_SESSION);
+  //session_destroy();
+  //session_write_close();
+  //header('Location: ../'.$ref);
+  //die;
 ?>
