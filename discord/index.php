acd26db3b202c86feeb18cd680c57c150814e05b
Turan Ates <jamesdusky@dustytavern.com>
2021-05-19 22:59:35 -0400

    Fixed href

--- discord/index.php
+++ discord/index.php
@@ -74,7 +74,7 @@ session_start();
         </div>
       </div>
       <div class="eleContainer">
-        <a href="/"class="navEle"><img src="../src/repHub.png" class="menuIcon"/><p>Reputation Hub</p></a>
+        <a href="../"class="navEle"><img src="../src/repHub.png" class="menuIcon"/><p>Reputation Hub</p></a>
         <a href="contracts" class="navEle"><img src="../src/contracts.png" class="menuIcon"/><p>Contracts</p></a>
         <a class="navEle navActive"><img src="../src/discord.png" class="menuIcon"/><p>Discord</p></a>
         <a href="faq" class="navEle"><img src="../src/faq.png" class="menuIcon"/><p>FAQ</p></a>
