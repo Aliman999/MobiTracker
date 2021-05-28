e54fc578b349a4b15c4e212a5d4753b84ab8bda7
Turan Ates <jamesdusky@dustytavern.com>
2021-05-28 03:41:51 -0400

    Debug

--- faq/index.php
+++ faq/index.php
@@ -179,6 +179,7 @@
       </div>
     </div>
   <script type="text/javascript" src="../js/socket.js"></script>
+  <script type="text/javascript" src="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "../js/nav.js";}else{echo "../js/nSession.js";} ?>" async></script>
   <script type="text/javascript" src="main.js"></script>
   </body>
 </html>
