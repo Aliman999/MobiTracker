56ba0b13b811fa81bb7749ee0d3c4fbcd16f722c
Turan Ates <jamesdusky@dustytavern.com>
2021-05-28 18:51:16 -0400

    Debug

--- gtemps/nav.php
+++ gtemps/nav.php
@@ -1,6 +1,6 @@
 <?php
 $active = basename($_SERVER['REQUEST_URI']);
-var_dump($active);
+$back = "";
 
 ?>
 <div id="navMenu" class="navMenu">
@@ -20,10 +20,10 @@ var_dump($active);
     </div>
   </div>
   <div class="eleContainer">
-    <a href="/" class="navEle"><img src="https://mobitracker.co/beta/src/repHub.png" class="menuIcon"/><p>Reputation Hub</p></a>
-    <a href="" class="navEle"><img src="https://mobitracker.co/beta/src/contracts.png" class="menuIcon"/><p>Contracts</p></a>
-    <a href="" class="navEle"><img src="https://mobitracker.co/beta/src/discord.png" class="menuIcon"/><p>Discord</p></a>
-    <a href="" class="navEle"><img src="https://mobitracker.co/beta/src/faq.png" class="menuIcon"/><p>FAQ</p></a>
+    <a href=" <?php if(!basename($_SERVER['REQUEST_URI']) == "beta"){echo "/";} ?>" class="navEle <?php if(basename($_SERVER['REQUEST_URI']) == "beta"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/repHub.png" class="menuIcon"/><p>Reputation Hub</p></a>
+    <a href=" <?php if(!basename($_SERVER['REQUEST_URI']) == "beta"){echo "../contracts";} ?>" class="navEle <?php if(basename($_SERVER['REQUEST_URI']) == "contracts"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/contracts.png" class="menuIcon"/><p>Contracts</p></a>
+    <a href=" <?php if(!basename($_SERVER['REQUEST_URI']) == "beta"){echo "../discord";} ?>" class="navEle <?php if(basename($_SERVER['REQUEST_URI']) == "discord"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/discord.png" class="menuIcon"/><p>Discord</p></a>
+    <a href=" <?php if(!basename($_SERVER['REQUEST_URI']) == "beta"){echo "../faq";} ?>" class="navEle <?php if(basename($_SERVER['REQUEST_URI']) == "faq"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/faq.png" class="menuIcon"/><p>FAQ</p></a>
     <a href="https://patreon.com/mobitracker" class="navEle"><img src="https://mobitracker.co/beta/src/patreon.png" class="menuIcon"/><p>Patreon</p></a>
   </div>
 </div>
