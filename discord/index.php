f2b3223ee6271ef88e5e7052b3e511bdb37f569d
Turan Ates <jamesdusky@dustytavern.com>
2021-05-28 03:39:01 -0400

    Debug

--- discord/index.php
+++ discord/index.php
@@ -58,9 +58,9 @@ session_start();
       </div>
       <div class="eleContainer">
         <a href="../" class="navEle"><img src="../src/repHub.png" class="menuIcon"/><p>Reputation Hub</p></a>
-        <a href="contracts" class="navEle"><img src="../src/contracts.png" class="menuIcon"/><p>Contracts</p></a>
+        <a href="../contracts" class="navEle"><img src="../src/contracts.png" class="menuIcon"/><p>Contracts</p></a>
         <a class="navEle navActive"><img src="../src/discord.png" class="menuIcon"/><p>Discord</p></a>
-        <a href="faq" class="navEle"><img src="../src/faq.png" class="menuIcon"/><p>FAQ</p></a>
+        <a href="../faq" class="navEle"><img src="../src/faq.png" class="menuIcon"/><p>FAQ</p></a>
         <a href="https://patreon.com/mobitracker" class="navEle"><img src="../src/patreon.png" class="menuIcon"/><p>Patreon</p></a>
       </div>
     </div>
