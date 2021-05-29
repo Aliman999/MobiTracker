49faecebefbbf475142c07454a08f1771c9d7523
Turan Ates <jamesdusky@dustytavern.com>
2021-05-28 20:42:42 -0400

    debug

--- gtemps/nav.php
+++ gtemps/nav.php
@@ -18,7 +18,7 @@ $base = basename($_SERVER['REQUEST_URI']);
     </div>
   </div>
   <div class="eleContainer">
-    <a href="<?php if($base != "beta"){echo "https://mobitracker.co/beta/"} ?>" class="navEle <?php if($base == "beta"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/repHub.png" class="menuIcon"/><p>Reputation Hub</p></a>
+    <a href="<?php if($base != "beta"){echo "https://mobitracker.co/beta/";} ?>" class="navEle <?php if($base == "beta"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/repHub.png" class="menuIcon"/><p>Reputation Hub</p></a>
     <a href="<?php if($base != "contracts"){echo "https://mobitracker.co/beta/contracts";}?>" class="navEle <?php if($base == "contracts"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/contracts.png" class="menuIcon"/><p>Contracts</p></a>
     <a href="<?php if($base != "discord"){echo "https://mobitracker.co/beta/discord";}?>" class="navEle <?php if($base == "discord"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/discord.png" class="menuIcon"/><p>Discord</p></a>
     <a href="<?php if($base != "faq"){echo "https://mobitracker.co/beta/faq";}?>" class="navEle <?php if($base == "faq"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/faq.png" class="menuIcon"/><p>FAQ</p></a>
