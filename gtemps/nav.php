49c26bc208840090a88c8a777b9c48838895a9ee
Turan Ates <jamesdusky@dustytavern.com>
2021-05-28 20:39:35 -0400

    Debug

--- gtemps/nav.php
+++ gtemps/nav.php
@@ -18,10 +18,10 @@ $base = basename($_SERVER['REQUEST_URI']);
     </div>
   </div>
   <div class="eleContainer">
-    <a href="<?php if($base == "beta"){echo "";}else if($base != "beta"){echo "../";} ?>" class="navEle <?php if($base == "beta"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/repHub.png" class="menuIcon"/><p>Reputation Hub</p></a>
-    <a href="<?php if($base == "contracts"){echo "";}else if($base == "beta"){echo "contracts";}else{echo "../contracts";} ?>" class="navEle <?php if($base == "contracts"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/contracts.png" class="menuIcon"/><p>Contracts</p></a>
-    <a href="<?php if($base == "discord"){echo "";}else if($base == "beta"){echo "discord";}else{echo "../discord";} ?>" class="navEle <?php if($base == "discord"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/discord.png" class="menuIcon"/><p>Discord</p></a>
-    <a href="<?php if($base == "faq"){echo "";}else if($base == "beta"){echo "faq";}else{echo "../faq";} ?>" class="navEle <?php if($base == "faq"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/faq.png" class="menuIcon"/><p>FAQ</p></a>
+    <a href="<?php if($base != "beta"){echo "https://mobitracker.co/beta/"} ?>" class="navEle <?php if($base == "beta"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/repHub.png" class="menuIcon"/><p>Reputation Hub</p></a>
+    <a href="<?php if($base != "contracts"){echo "https://mobitracker.co/beta/contracts";}?>" class="navEle <?php if($base == "contracts"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/contracts.png" class="menuIcon"/><p>Contracts</p></a>
+    <a href="<?php if($base != "discord"){echo "https://mobitracker.co/beta/discord";}?>" class="navEle <?php if($base == "discord"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/discord.png" class="menuIcon"/><p>Discord</p></a>
+    <a href="<?php if($base != "faq"){echo "https://mobitracker.co/beta/faq";}?>" class="navEle <?php if($base == "faq"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/faq.png" class="menuIcon"/><p>FAQ</p></a>
     <a href="https://patreon.com/mobitracker" class="navEle"><img src="https://mobitracker.co/beta/src/patreon.png" class="menuIcon"/><p>Patreon</p></a>
   </div>
 </div>
