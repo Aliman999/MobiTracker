236e97b5495b6e0555d7921a01738d95dbc76496
Turan Ates <jamesdusky@dustytavern.com>
2021-05-28 20:30:46 -0400

    Debug

--- gtemps/nav.php
+++ gtemps/nav.php
@@ -20,9 +20,9 @@ echo $base;
   </div>
   <div class="eleContainer">
     <a href="<?php if($base == "beta"){echo "";}else if($base != "beta"){echo "../";} ?>" class="navEle <?php if($base == "beta"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/repHub.png" class="menuIcon"/><p>Reputation Hub</p></a>
-    <a href="<?php if($base == "contracts"){echo "../contracts";}else if($base == "beta"){echo "contracts";}else{echo "";} ?>" class="navEle <?php if($base == "contracts"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/contracts.png" class="menuIcon"/><p>Contracts</p></a>
-    <a href="<?php if($base == "discord"){echo "../discord";}else if($base == "beta"){echo "discord";} ?>" class="navEle <?php if($base == "discord"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/discord.png" class="menuIcon"/><p>Discord</p></a>
-    <a href="<?php if($base == "faq"){echo "../faq";}else if($base == "beta"){echo "faq";} ?>" class="navEle <?php if($base == "faq"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/faq.png" class="menuIcon"/><p>FAQ</p></a>
+    <a href="<?php if($base == "contracts"){echo "";}else if($base == "beta"){echo "contracts";}else{echo "../contracts";} ?>" class="navEle <?php if($base == "contracts"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/contracts.png" class="menuIcon"/><p>Contracts</p></a>
+    <a href="<?php if($base == "discord"){echo "";}else if($base == "beta"){echo "discord";} ?>" class="navEle <?php if($base == "discord"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/discord.png" class="menuIcon"/><p>Discord</p></a>
+    <a href="<?php if($base == "faq"){echo "";}else if($base == "beta"){echo "faq";} ?>" class="navEle <?php if($base == "faq"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/faq.png" class="menuIcon"/><p>FAQ</p></a>
     <a href="https://patreon.com/mobitracker" class="navEle"><img src="https://mobitracker.co/beta/src/patreon.png" class="menuIcon"/><p>Patreon</p></a>
   </div>
 </div>
