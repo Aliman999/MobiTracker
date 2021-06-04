2a0ac7e0b2e0c3f9c095d3ecfd67d4dde9ad9e7f
Turan Ates <jamesdusky@dustytavern.com>
2021-06-04 12:58:57 -0400

    Debug

--- settings/index.php
+++ settings/index.php
@@ -70,7 +70,7 @@
           <p>Username: <?php echo $_SESSION['username'] ?></p>
           <p>Email: N/A</p>
           <div>
-            <p>Password: ⬤⬤⬤⬤⬤⬤⬤⬤</p> <a class="rButton highlight-green">Change Password</a>
+            <p>Password: ●●●●●●●●●●●●</p> <a class="rButton highlight-green">Change Password</a>
           </div>
         </div>
       </div>
