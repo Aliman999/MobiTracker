2fe6099d98ed982959cc893509d5969c1077d7b3
Turan Ates <jamesdusky@dustytavern.com>
2021-06-07 10:22:08 -0400

    Debug

--- settings/discord/index.php
+++ settings/discord/index.php
@@ -52,7 +52,7 @@
             echo "<a id='lmSBtn' class='verify_btn'>Sign Up</a>";
           }
           if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-            echo "<div class='userContainer'><img class='verified' src='../src/verified.png'><p class='loginName'>".$_SESSION['username']."</p></div>";
+            echo "<div class='userContainer'><img class='verified' src='../../src/verified.png'><p class='loginName'>".$_SESSION['username']."</p></div>";
           }
           ?>
           <div id="navBtnContainer" class="navBtnContainer">
