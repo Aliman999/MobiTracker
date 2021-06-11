c11724a6273e1ab5d45dfb8f34f952ad8c1e1d85
Turan Ates <jamesdusky@dustytavern.com>
2021-06-11 00:49:22 -0400

    Debug

--- settings/settings.php
+++ settings/settings.php
@@ -1,9 +1,27 @@
 <?php
 session_start();
 if(!isset($_SESSION['loggedin'])){
-  exit();
+  //exit("403 Forbidden.");
 }
 $accepted = ["email", "password", "discord", "privacy", "displayname", "referrals"];
-if(in_array($_SESSION['activeSetting'], $accepted)){
-  echo "accepted";
+echo $_SERVER['HTTP_REFERER'];
+
+if(!in_array($_SESSION['activeSetting'], $accepted)){
+  //header('Location: https://mobitracker.co/');
+}
+
+if($_POST['setting'] == 'email'){
+
+}elseif($_POST['setting'] == 'password'){
+
+}elseif($_POST['setting'] == 'discord'){
+
+}elseif($_POST['setting'] == 'privacy'){
+
+}elseif($_POST['setting'] == 'displayname'){
+
+}elseif($_POST['setting'] == 'referrals'){
+
+}else{
+  //exit("403 Forbidden.");
 }
