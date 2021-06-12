e3cfb94e7cf6c5e3159778cbc26632ca485dcd0a
Turan Ates <jamesdusky@dustytavern.com>
2021-06-11 21:16:29 -0400

    Debug

--- privacypolicy/index.php
+++ privacypolicy/index.php
@@ -68,12 +68,10 @@ if (empty($_SESSION['token'])) {
         <p class="webSlogan">Last updated: June 11, 2021</p>
       </div>
       <div class="container container-header" id="general">
-        <header class="playercard">
-          <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
-          <p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy.</p>
-          <p class="ppTitle">Interpretation and Definitions</p>
-          <p class="ppSubTitle">Interpretation</p>
-        </header>
+        <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
+        <p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy.</p>
+        <p class="ppTitle">Interpretation and Definitions</p>
+        <p class="ppSubTitle">Interpretation</p>
       </div>
     </div>
     <?php include "../gtemps/footer.php"; ?>
