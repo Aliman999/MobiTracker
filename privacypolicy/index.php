b2f07561830630585db518f7f26c5e186628884e
Turan Ates <jamesdusky@dustytavern.com>
2021-06-11 21:45:52 -0400

    Debug

--- privacypolicy/index.php
+++ privacypolicy/index.php
@@ -139,6 +139,32 @@ if (empty($_SESSION['token'])) {
           <p>For more information about the cookies we use and your choices regarding cookies, please visit our Cookies Policy or the Cookies section of our Privacy Policy.</p>
           <p class="ppSubTitle">Use of Your Personal Data</p>
           <p>The Company may use Personal Data for the following purposes:</p>
+          <ul>
+            <li>
+              <strong>To provide and maintain our Service</strong>, including to monitor the usage of our Service.
+            </li>
+            <li>
+              <strong>To manage Your Account:</strong> to manage Your registration as a user of the Service. The Personal Data You provide can give You access to different functionalities of the Service that are available to You as a registered user.
+            </li>
+            <li>
+              <strong>For the performance of a contract:</strong> the development, compliance and undertaking of the purchase contract for the products, items or services You have purchased or of any other contract with Us through the Service.
+            </li>
+            <li>
+              <strong>To contact You:</strong> To contact You by email, telephone calls, SMS, or other equivalent forms of electronic communication, such as a mobile application's push notifications regarding updates or informative communications related to the functionalities, products or contracted services, including the security updates, when necessary or reasonable for their implementation.
+            </li>
+            <li>
+              <strong>To provide You</strong> with news, special offers and general information about other goods, services and events which we offer that are similar to those that you have already purchased or enquired about unless You have opted not to receive such information.
+            </li>
+            <li>
+              <strong>To manage Your requests:</strong> To attend and manage Your requests to Us.
+            </li>
+            <li>
+              <strong>For business transfers:</strong> We may use Your information to evaluate or conduct a merger, divestiture, restructuring, reorganization, dissolution, or other sale or transfer of some or all of Our assets, whether as a going concern or as part of bankruptcy, liquidation, or similar proceeding, in which Personal Data held by Us about our Service users is among the assets transferred.
+            </li>
+            <li>
+              <strong>For other purposes</strong>: We may use Your information for other purposes, such as data analysis, identifying usage trends, determining the effectiveness of our promotional campaigns and to evaluate and improve our Service, products, services, marketing and your experience.
+            </li>
+          </ul>
         </header>
       </div>
     </div>
