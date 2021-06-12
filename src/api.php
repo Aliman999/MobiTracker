70cf6536a65b1860f14f7bf3ceb56466010e6628
Turan Ates <jamesdusky@dustytavern.com>
2021-06-12 19:56:41 -0400

    Debug

--- src/api.php
+++ src/api.php
@@ -85,6 +85,7 @@ if (isset($headers)) {
       }
       //var_dump($checkOrgDif);
       if($forceOrgRefresh == true || $checkOrg == 1){
+        echo "updating org";
         include "api_userOrg.php";
       }
     //}
