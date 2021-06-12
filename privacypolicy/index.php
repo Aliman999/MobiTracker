45f62df8a50a293b10f97df75c83f89d93b1990c
Turan Ates <jamesdusky@dustytavern.com>
2021-06-11 21:53:45 -0400

    Debug

--- privacypolicy/index.php
+++ privacypolicy/index.php
@@ -194,6 +194,23 @@ if (empty($_SESSION['token'])) {
             <li>Protect the personal safety of Users of the Service or the public</li>
             <li>Protect against legal liability</li>
           </ul>
+          <p class="ppSubTitle">Security of Your Personal Data</p>
+          <p>The security of Your Personal Data is important to Us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While We strive to use commercially acceptable means to protect Your Personal Data, We cannot guarantee its absolute security.</p>
+          <p class="ppTitle">Children's Privacy</p>
+          <p>Our Service does not address anyone under the age of 13. We do not knowingly collect personally identifiable information from anyone under the age of 13. If You are a parent or guardian and You are aware that Your child has provided Us with Personal Data, please contact Us. If We become aware that We have collected Personal Data from anyone under the age of 13 without verification of parental consent, We take steps to remove that information from Our servers.</p>
+          <p>If We need to rely on consent as a legal basis for processing Your information and Your country requires consent from a parent, We may require Your parent's consent before We collect and use that information.</p>
+          <p class="ppTitle">Links to Other Websites</p>
+          <p>Our Service may contain links to other websites that are not operated by Us. If You click on a third party link, You will be directed to that third party's site. We strongly advise You to review the Privacy Policy of every site You visit.</p>
+          <p>We have no control over and assume no responsibility for the content, privacy policies or practices of any third party sites or services.</p>
+          <p class="ppTitle">Changes to this Privacy Policy</p>
+          <p>We may update Our Privacy Policy from time to time. We will notify You of any changes by posting the new Privacy Policy on this page.</p>
+          <p>We will let You know via email and/or a prominent notice on Our Service, prior to the change becoming effective and update the &quot;Last updated&quot; date at the top of this Privacy Policy.</p>
+          <p>You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.</p>
+          <p class="ppTitle">Contact Us</p>
+          <p>If you have any questions about this Privacy Policy, You can contact us:</p>
+          <ul>
+            <li>By email: admin@mobitracker.co</li>
+          </ul>
         </header>
       </div>
     </div>
