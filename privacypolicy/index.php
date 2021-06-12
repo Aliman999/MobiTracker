dd5a2b53a1be8c3945a583ec2be9d6313fbde4ef
Turan Ates <jamesdusky@dustytavern.com>
2021-06-11 21:43:14 -0400

    Debug

--- privacypolicy/index.php
+++ privacypolicy/index.php
@@ -115,6 +115,27 @@ if (empty($_SESSION['token'])) {
             <li><strong>Web Beacons.</strong> Certain sections of our Service and our emails may contain small electronic files known as web beacons (also referred to as clear gifs, pixel tags, and single-pixel gifs) that permit the Company, for example, to count users who have visited those pages or opened an email and for other related website statistics (for example, recording the popularity of a certain section and verifying system and server integrity).</li>
           </ul>
           <p>Cookies can be &quot;Persistent&quot; or &quot;Session&quot; Cookies. Persistent Cookies remain on Your personal computer or mobile device when You go offline, while Session Cookies are deleted as soon as You close Your web browser. Learn more about cookies: <a class="highlight underline" href="https://www.privacypolicies.com/blog/cookies/" target="_blank">What Are Cookies?</a>.</p>
+          <p>We use both Session and Persistent Cookies for the purposes set out below:</p>
+          <ul>
+            <li>
+              <p><strong>Necessary / Essential Cookies</strong></p>
+              <p>Type: Session Cookies</p>
+              <p>Administered by: Us</p>
+              <p>Purpose: These Cookies are essential to provide You with services available through the Website and to enable You to use some of its features. They help to authenticate users and prevent fraudulent use of user accounts. Without these Cookies, the services that You have asked for cannot be provided, and We only use these Cookies to provide You with those services.</p>
+            </li>
+            <li>
+              <p><strong>Cookies Policy / Notice Acceptance Cookies</strong></p>
+              <p>Type: Persistent Cookies</p>
+              <p>Administered by: Us</p>
+              <p>Purpose: These Cookies identify if users have accepted the use of cookies on the Website.</p>
+            </li>
+            <li>
+              <p><strong>Functionality Cookies</strong></p>
+              <p>Type: Persistent Cookies</p>
+              <p>Administered by: Us</p>
+              <p>Purpose: These Cookies allow us to remember choices You make when You use the Website, such as remembering your login details or language preference. The purpose of these Cookies is to provide You with a more personal experience and to avoid You having to re-enter your preferences every time You use the Website.</p>
+            </li>
+          </ul>
         </header>
       </div>
     </div>
