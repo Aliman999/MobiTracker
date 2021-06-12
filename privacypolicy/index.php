2b7e8f727e4c27755adaab35d8a73f40132a3c35
Turan Ates <jamesdusky@dustytavern.com>
2021-06-11 21:15:49 -0400

    Debug

--- privacypolicy/index.php
+++ privacypolicy/index.php
@@ -71,7 +71,8 @@ if (empty($_SESSION['token'])) {
         <header class="playercard">
           <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
           <p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy.</p>
-          <h2>Interpretation and Definitions</h2>
+          <p class="ppTitle">Interpretation and Definitions</p>
+          <p class="ppSubTitle">Interpretation</p>
         </header>
       </div>
     </div>
