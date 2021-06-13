faaa6c576f8363da080be22efa77b2d8d52be429
Turan Ates <jamesdusky@dustytavern.com>
2021-06-12 20:35:59 -0400

    Debug

--- js/main.js
+++ js/main.js
@@ -161,7 +161,7 @@ function showPlayer(node, ns){
   hideSB();
   clearSB();
   dataCount = 0;
-  if(!player){
+  if(node != name){
     request.open("GET", "src/api.php"+"?username="+node+"&v="+ext);
     if(ext == "live"){
       ext = "auto";
