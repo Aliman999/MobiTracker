e4c3d27363843e5f669dc3aae6dfc76879eef815
Turan Ates <jamesdusky@dustytavern.com>
2021-06-13 23:50:20 -0400

    Changed ref to live server

--- gtemps/nav.php
+++ gtemps/nav.php
@@ -4,7 +4,7 @@ $base = basename($_SERVER['REQUEST_URI']);
 <div id="navMenu" class="navMenu">
   <div class="navTitleContainer">
     <p class="navTitle">NAVIGATE</p>
-    <a href="javascript:void(0)" class="closebtn" id="closeNavBtn"><img src="https://mobitracker.co/beta/src/close.png" class="navBtn"></a>
+    <a href="javascript:void(0)" class="closebtn" id="closeNavBtn"><img src="https://mobitracker.co/src/close.png" class="navBtn"></a>
   </div>
   <div class="miniPlayerDivOut">
     <div class="miniPlayerOut">
@@ -18,11 +18,11 @@ $base = basename($_SERVER['REQUEST_URI']);
     </div>
   </div>
   <div class="eleContainer">
-    <a href="<?php if($base != "beta"){echo "https://mobitracker.co/beta/";} ?>" class="navEle <?php if($base == "beta"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/repHub.png" class="menuIcon"/><p>Reputation Hub</p></a>
-    <a href="<?php if($base != "contracts"){echo "https://mobitracker.co/beta/contracts";}?>" class="navEle <?php if($base == "contracts"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/contracts.png" class="menuIcon"/><p>Contracts</p></a>
-    <a href="<?php if($base != "discord"){echo "https://mobitracker.co/beta/discord";}?>" class="navEle <?php if($base == "discord"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/discord.png" class="menuIcon"/><p>Discord</p></a>
-    <a href="<?php if($base != "faq"){echo "https://mobitracker.co/beta/faq";}?>" class="navEle <?php if($base == "faq"){echo "navActive";} ?>"><img src="https://mobitracker.co/beta/src/faq.png" class="menuIcon"/><p>FAQ</p></a>
-    <a href="https://patreon.com/mobitracker" class="navEle"><img src="https://mobitracker.co/beta/src/patreon.png" class="menuIcon"/><p>Patreon</p></a>
+    <a href="<?php if($base != "beta"){echo "https://mobitracker.co/";} ?>" class="navEle <?php if($base == "beta"){echo "navActive";} ?>"><img src="https://mobitracker.co/src/repHub.png" class="menuIcon"/><p>Reputation Hub</p></a>
+    <a href="<?php if($base != "contracts"){echo "https://mobitracker.co/contracts";}?>" class="navEle <?php if($base == "contracts"){echo "navActive";} ?>"><img src="https://mobitracker.co/src/contracts.png" class="menuIcon"/><p>Contracts</p></a>
+    <a href="<?php if($base != "discord"){echo "https://mobitracker.co/discord";}?>" class="navEle <?php if($base == "discord"){echo "navActive";} ?>"><img src="https://mobitracker.co/src/discord.png" class="menuIcon"/><p>Discord</p></a>
+    <a href="<?php if($base != "faq"){echo "https://mobitracker.co/faq";}?>" class="navEle <?php if($base == "faq"){echo "navActive";} ?>"><img src="https://mobitracker.co/src/faq.png" class="menuIcon"/><p>FAQ</p></a>
+    <a href="https://patreon.com/mobitracker" class="navEle"><img src="https://mobitracker.co/src/patreon.png" class="menuIcon"/><p>Patreon</p></a>
   </div>
 </div>
 <div id="navBackDrop" class="navBackDrop"></div> <!-- REQUIRED FOR NAV -->
