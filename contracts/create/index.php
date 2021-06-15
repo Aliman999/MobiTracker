b499469fdac7a0264a351d423356848f8f1b754d
Turan Ates <jamesdusky@dustytavern.com>
2021-06-15 01:02:45 -0400

    Debug

--- contracts/create/index.php
+++ contracts/create/index.php
@@ -18,22 +18,31 @@ if(!in_array($_GET['service'], $service)){
     <meta charset="utf-8">
     <title>Contracts - MobiTracker</title>
     <link href="https://fonts.googleapis.com/css2?family=Exo:wght@300;400;500;600&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://mobitracker.co/css/style.css">
-    <link rel="stylesheet" href="https://mobitracker.co/contracts/locale.css">
-    <link rel="stylesheet" href="https://mobitracker.co/contracts/create/locale.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/patreon.css">
+    <link rel="stylesheet" href="../css/nav.css">
+    <link rel="stylesheet" href="locale.css">
     <link rel="apple-touch-icon" sizes="180x180" href="https://mobitracker.co/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="https://mobitracker.co/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="https://mobitracker.co/favicon-16x16.png">
     <link rel="manifest" href="https://mobitracker.co/site.webmanifest">
     <link rel="mask-icon" href="https://mobitracker.co/safari-pinned-tab.svg" color="#5bbad5">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Contracts - MobiTracker.co" />
+    <meta property="og:description" content="Find who to trust when you need a hand to lead your armed escort or who to avoid letting on your ship" />
+    <meta property="og:url" content="https://mobitracker.co/contracts" />
+    <meta property="og:image" content="https://mobitracker.co/android-chrome-512x512.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#253139">
     <?php
     echo "<meta name='token' content=".$_SESSION['token'].">";
+    if($_SESSION['loggedin'] == true){
+      echo "<meta name='jwt' content=".$_SESSION['jwt'].">";
+    }
     ?>
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-177343701-1"></script>
-    <script type="text/javascript" src="https://mobitracker.co/js/ga.js"></script>
+    <script type="text/javascript" src="../js/ga.js"></script>
   </head>
   <body>
     <?php include "../../gtemps/nav.php"; ?>
