d98b475f107c42ff0ab23b8fc0fd9d14dc4de0c8
Turan Ates <jamesdusky@dustytavern.com>
2021-06-18 19:33:01 -0400

    Debug

--- settings/index.php
+++ settings/index.php
@@ -85,7 +85,7 @@
             <div class="">
 
             </div>
-            <p><span class="rBold">Experience:</span> <span class="subP"><?php echo $row['xp'] ?></span></p>
+            <p><span class="rBold">Experience:</span><br><span class="subP"><?php echo $row['xp'] ?></span></p>
           </div>
           <div>
             <p><span class="rBold">Email:</span><br><span class="subP"><?php echo $row['email'] ?></span></p>
