805d7fcaaeef9a71f0e3d19d9374f6a332b7b482
Turan Ates <jamesdusky@dustytavern.com>
2021-06-18 10:03:59 -0400

    Testing

--- settings/settings.php
+++ settings/settings.php
@@ -4,20 +4,16 @@ session_start();
 $headers = $_SERVER['HTTP_TOKEN'];
 
 if(isset($headers)){
-  if ($headers !== $_SESSION['token']){
-    exit(json_encode(['error' => 'Wrong token.']));
-  }else{
-    function init(){
-      $sql = "SELECT * FROM players WHERE username = '".$_SESSION['username']."';";
-      $result = mysqli_query($link, $sql);
-      $emparray = array();
-      while($row = mysqli_fetch_assoc($result)){
-        $emparray[] = $row;
-      }
-      var_dump($emparray);
+  function init(){
+    $sql = "SELECT * FROM players WHERE username = '".$_SESSION['username']."';";
+    $result = mysqli_query($link, $sql);
+    $emparray = array();
+    while($row = mysqli_fetch_assoc($result)){
+      $emparray[] = $row;
     }
-    init();
+    var_dump($emparray);
   }
+  init();
 }else{
    exit(json_encode(['error' => 'No token.']));
 }
