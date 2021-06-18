38f5c95c9287bf8f14d304a34fbbf6a42881c34b
Turan Ates <jamesdusky@dustytavern.com>
2021-06-18 14:33:48 -0400

    Debug

--- settings/settings.php
+++ settings/settings.php
@@ -9,6 +9,8 @@ include "../src/config.php";
 $headers = $_SESSION['token'];
 if(isset($headers)){
   $sql = "SELECT * FROM players INNER JOIN discord ON discord.username LIKE CONCAT('%', '".$_SESSION['username']."', '%') WHERE players.username = '".$_SESSION['username']."';";
+  echo $sql;
+  /*
   $result = mysqli_query($link, $sql);
   $row = mysqli_fetch_assoc($result);
   function xp($rep){
@@ -47,6 +49,7 @@ if(isset($headers)){
     $row['email'] = "Encrypted";
   }
   var_dump($row);
+  */
 }else{
    exit(json_encode(['error' => 'No token.']));
 }
