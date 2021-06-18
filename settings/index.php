9843686f7991df93f2aec59991bf0628ecd3e8a5
Turan Ates <jamesdusky@dustytavern.com>
2021-06-18 10:38:24 -0400

    Debug

--- settings/index.php
+++ settings/index.php
@@ -85,7 +85,7 @@
             <p><span class="rBold">Experience:</span><br><span class="subP"><?php echo $row['xp'] ?></span></p>
           </div>
           <div>
-            <p><span class="rBold">Verified:</span><br><span class="subP"><?php echo $_SESSION['verifiy'] ?></span></p>
+            <p><span class="rBold">Verified:</span><br><span class="subP"><?php echo $_SESSION['verify'] ?></span></p>
             <a class="rButton highlight rightSide">Add RSI</a>
           </div>
           <div>
