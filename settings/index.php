47f2369d6956641e159e9408a62bf324c8288cc9
Turan Ates <jamesdusky@dustytavern.com>
2021-06-18 18:01:51 -0400

    Debug

--- settings/index.php
+++ settings/index.php
@@ -85,7 +85,7 @@
             <p><span class="rBold">Experience:</span><br><span class="subP"><?php echo $row['xp'] ?></span></p>
           </div>
           <div>
-            <p><span class="rBold">Verified:</span><br><span class="subP <?php echo $row['verifyClass'] ?>"><?php echo $row['verify'] ?></span></p>
+            <p><span class="rBold">Verification:</span><br><span class="subP <?php echo $row['verifyClass'] ?>"><?php echo $row['verify'] ?></span></p>
             <?php
             if($row['verify'] !== 'Verified'){
               echo '<a class="rButton highlight rightSide">Verify</a>';
