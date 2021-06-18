4503f0bf1de167d08c94fc09311932b0633a73e9
Turan Ates <jamesdusky@dustytavern.com>
2021-06-18 10:05:15 -0400

    Debug

--- settings/settings.php
+++ settings/settings.php
@@ -4,13 +4,13 @@ session_start();
 $headers = $_SESSION['token'];
 
 if(isset($headers)){
-    $sql = "SELECT * FROM players WHERE username = '".$_SESSION['username']."';";
-    $result = mysqli_query($link, $sql);
-    $emparray = array();
-    while($row = mysqli_fetch_assoc($result)){
-      $emparray[] = $row;
-    }
-    var_dump($emparray);
+  $sql = "SELECT * FROM players WHERE username = '".$_SESSION['username']."';";
+  $result = mysqli_query($link, $sql);
+  $emparray = array();
+  while($row = mysqli_fetch_assoc($result)){
+    $emparray[] = $row;
+  }
+  echo $sql;
 }else{
    exit(json_encode(['error' => 'No token.']));
 }
