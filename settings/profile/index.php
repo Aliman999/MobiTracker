0d824f32b5ba8b8f3ed1a7dcf54cd216fc813d14
Turan Ates <jamesdusky@dustytavern.com>
2021-06-19 17:33:27 -0400

    Debug

--- settings/profile/index.php
+++ settings/profile/index.php
@@ -5,43 +5,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once "../vendor/autoload.php";
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-function sendMail($email){
-  $mail = new PHPMailer;
-  try {
-    $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'mail.privateemail.com';  // Specify main and backup SMTP servers
-    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'admin@dustytavern.com';                 // SMTP username
-    $mail->Password = 'Fallenlegion5';                           // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
-    $mail->From = 'admin@dustytavern.com';
-    $mail->FromName = 'MobiTracker';
-    $mail->addAddress($email, $username);     // Add a recipient
-    $mail->addReplyTo('admin@dustytavern.com', 'Information');
-
-    $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
-    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-    $mail->isHTML(true);                                  // Set email format to HTML
-
-    $mail->Subject = 'Password Reset';
-    $mail->Body    = '<head><style>body{margin: 0 auto;background-color: #151f26;padding-bottom: 32px;color: #C4D7E6;font-family: "Exo", sans-serif;font-weight: 300;text-align: center;outline: 0;}a, a:visited, a:active, a:link{color: #C4D7E6;text-decoration: none;outline: 0;}a:hover{color: #fff;}.container{position: relative;display: inline-block;width: auto;margin: auto;top: 128px;background-color: #253139;padding: 40px 25px;border: 2px solid rgb(57, 206, 216);box-shadow: 0px 0px 15px rgba(57, 206, 216, 0.5);border-radius: 4px;word-wrap: break-word;}</style></head><body><div class="container"><h1>Verification Code</h1><p>'.$password['password'].'</p> <br><br><p>Someone requested to reset your password. If it wasnt you please ignore this. <br><br> If it persists please reply and message Aliman#5518 on discord<br><br>Copy and paste this code at the link provided: <a href="https://mobitracker.co/resetpassword/" style="font-weight: bold;">Reset Password</a></p></div></body>';
-
-    $mail->send();
-    $emailConfirm = 'Successfully sent to ' .$email;
-
-    header( "refresh:3;" );
-  }catch (Exception $e) {
-    echo $email_err"Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-  }
-}
-
 if(empty($_SESSION['loggedin']) || $_SESSION['loggedin'] === false){
   header("location: ../");
 }else{
@@ -123,7 +86,7 @@ if(empty($_SESSION['loggedin']) || $_SESSION['loggedin'] === false){
         <div class="setting">
           <?php
           if($row['email'] === "Encrypted"){
-            echo '<div><p><span class="rBold">Your email is encrypted.</span></p></div>';
+            echo '<div><p><span class="rBold">Your email is encrypted</span></p></div>';
           }
           ?>
           <div>
