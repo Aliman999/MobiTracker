417b98c6a9cd62683cccd87ebfa296ab4cddb00e
Turan Ates <jamesdusky@dustytavern.com>
2021-06-19 22:22:02 -0400

    Debugging

--- index.php
+++ index.php
@@ -108,8 +108,8 @@
       </div>
     </div>
     <?php include "gtemps/footer.php"; ?>
-    <script type="text/javascript" src="js/socket.js"></script>
-    <script type="text/javascript" src="js/main.js" async></script>
-    <script type="text/javascript" src="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "js/nav.js";}else{echo "js/nSession.js";} ?>" async></script>
+    <script type="text/javascript" src="js/socket.js?date=6/19"></script>
+    <script type="text/javascript" src="js/main.js?date=6/19" async></script>
+    <script type="text/javascript" src="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "js/nav.js?date=6/19";}else{echo "js/nSession.js?date=6/19";} ?>" async></script>
   </body>
 </html>
