1f90fcd4a5adaa3e4a978794dd11326686af88cd
Turan Ates <jamesdusky@dustytavern.com>
2021-06-21 13:23:19 -0400

    Debug

--- settings/email/main.js
+++ settings/email/main.js
@@ -1,5 +1,13 @@
 'use strict';
 var eBtn = document.getElementById("email");
 eBtn.onclick = function(){
-
+  var verifyEmail = new XMLHttpRequest();
+  verifyEmail.open("POST", "sendEmail.php");
+  verifyEmail.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  verifyEmail.setRequestHeader(tokenHeader.name,tokenHeader.content);
+  verifyEmail.responseType = "json";
+  verifyEmail.send("email=jamesdusky@dustytavern.com");
+  verifyEmail.onload = function() {
+    var userResponse = verifyEmail.response;
+  }
 }
