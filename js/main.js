583eae459bf58db304cbb0f2d49c19e9bf34e14a
Turan Ates <jamesdusky@dustytavern.com>
2021-06-20 23:47:35 -0400

    Debug

--- js/main.js
+++ js/main.js
@@ -168,7 +168,7 @@ function showPlayer(node){
       ext = "auto";
     }
     request.setRequestHeader(tokenHeader.name,tokenHeader.content);
-    request.responseType = "json";
+    //request.responseType = "json";
     request.send();
     request.onload = function() {
       player = request.response;
