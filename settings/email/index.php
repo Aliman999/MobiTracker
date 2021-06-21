5000fe0f382e71d81517c5baaaf62f8df2a2d55d
Turan Ates <jamesdusky@dustytavern.com>
2021-06-21 14:43:51 -0400

    Debuig

--- settings/email/index.php
+++ settings/email/index.php
@@ -96,7 +96,8 @@ if(empty($_SESSION['loggedin']) || $_SESSION['loggedin'] === false){
           <div>
             <p><span class="rBold">Email:</span><br><span class="subP"><?php echo $row['email'] ?></span></p>
             <a class="rButton highlight rightSide" id="email">Change Email</a>
-            <p class="hidden"></p>
+            <input type="text" value="" placeholder="Enter your Email.">
+            <p class="hidden" id="status"></p>
           </div>
         </div>
       </div>
