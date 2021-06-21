2b1c8c3e328da72899cb5eee3fd384682cb84802
Turan Ates <jamesdusky@dustytavern.com>
2021-06-21 14:46:51 -0400

    Debug

--- settings/email/main.js
+++ settings/email/main.js
@@ -4,6 +4,7 @@ var tokenHeader = document.getElementsByName("token")[0];
 var input = document.getElementById("emailInput");
 var status = document.getElementById("status");
 eBtn.onclick = function(){
+  /*
   var verifyEmail = new XMLHttpRequest();
   verifyEmail.open("POST", "sendEmail.php");
   verifyEmail.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
@@ -11,10 +12,11 @@ eBtn.onclick = function(){
   //verifyEmail.responseType = "json";
   verifyEmail.send("email=jamesdusky@dustytavern.com");
   verifyEmail.onload = function() {
-    var userResponse = verifyEmail.response;
-    if(status.classList.contains("hidden")){
-      status.classList.toggle("hidden");
-      status.innerText = "test";
-    }
+  }
+  */
+  var userResponse = verifyEmail.response;
+  if(status.classList.contains("hidden")){
+    status.classList.toggle("hidden");
+    status.innerText = "test";
   }
 }
