111a0380e5ba4c52489752daae7a2e7fc15e1707
Turan Ates <jamesdusky@dustytavern.com>
2021-06-21 18:21:44 -0400

    Debug

--- settings/email/main.js
+++ settings/email/main.js
@@ -3,29 +3,32 @@ var eBtn = document.getElementById("email");
 var tokenHeader = document.getElementsByName("token")[0];
 var input = document.getElementById("emailInput");
 var rStatus = document.getElementById("status");
-var email;
 
 eBtn.onclick = function(){
   if(!input.classList.contains("hidden")){
-    var verifyEmail = new XMLHttpRequest();
-    verifyEmail.open("POST", "sendEmail.php");
-    verifyEmail.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    verifyEmail.setRequestHeader(tokenHeader.name,tokenHeader.content);
-    verifyEmail.responseType = "json";
-    verifyEmail.send("email="+email);
-    verifyEmail.onload = function() {
-      var userResponse = verifyEmail.response;
-      if(userResponse.status){
-        if(rStatus.classList.contains("hidden")){
-          rStatus.classList.toggle("hidden");
-          rStatus.innerText = "test";
-        }
-        setTimeout(()=>{
-          rStatus.classList.add("hidden");
-        }, 2000);
-      }
-    }
+    verify(input.value);
   }else{
     input.classList.remove("hidden");
+  }
+}
+
+function verify(email){
+  var verifyEmail = new XMLHttpRequest();
+  verifyEmail.open("POST", "sendEmail.php");
+  verifyEmail.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  verifyEmail.setRequestHeader(tokenHeader.name,tokenHeader.content);
+  verifyEmail.responseType = "json";
+  verifyEmail.send("email="+email);
+  verifyEmail.onload = function() {
+    var userResponse = verifyEmail.response;
+    if(userResponse.status){
+      if(rStatus.classList.contains("hidden")){
+        rStatus.classList.toggle("hidden");
+        rStatus.innerText = "test";
+      }
+      setTimeout(()=>{
+        rStatus.classList.add("hidden");
+      }, 2000);
+    }
   }
 }
