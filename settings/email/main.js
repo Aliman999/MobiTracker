6437c2664fdc9ba6e13506f9e734eac5149ac441
Turan Ates <jamesdusky@dustytavern.com>
2021-06-24 21:09:58 -0400

    Debug

--- settings/email/main.js
+++ settings/email/main.js
@@ -7,25 +7,22 @@ var input = document.getElementsByClassName("userInput")[0];
 var submit = document.getElementById("submit");
 
 eBtn.onclick = function(){
-  submit.onclick = function(){
-    if(!eContainer.classList.contains("hidden")){
+  eContainer.classList.remove("hidden");
+  eContainer.focus();
+  eContainer.addEventListener('keypress', function (e){
+    if(e.key === 'Enter'){
       verify(eContainer.value);
-    }else{
-      eContainer.classList.remove("hidden");
-      eContainer.focus();
-      eContainer.addEventListener('keypress', function (e){
-        if(e.key === 'Enter'){
-          verify(eContainer.value);
-        }
-      });
-      input.oninput = function(e){
-        if(e.target.value.includes("@")){
-          eContainer.verified = true;
-        }else{
-          eContainer.verified = false;
-        }
-      }
     }
+  });
+  input.oninput = function(e){
+    if(e.target.value.includes("@")){
+      eContainer.verified = true;
+    }else{
+      eContainer.verified = false;
+    }
+  }
+  submit.onclick = function(){
+    verify(eContainer.value);
   }
 }
 
