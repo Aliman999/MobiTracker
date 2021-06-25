a3876820ac595faea9f1769e8553936e14f8ded4
Turan Ates <jamesdusky@dustytavern.com>
2021-06-24 20:59:42 -0400

    Debug

--- settings/email/index.php
+++ settings/email/index.php
@@ -80,11 +80,10 @@ if(empty($_SESSION['loggedin']) || $_SESSION['loggedin'] === false){
         <div class="settingsNav">
           <a href="../" class="highlight nactive">Overview</a>
           <a href="../profile" class="highlight nactive">RSI Profile</a>
+          <a href="../security" class="highlight nactive">Security</a>
           <a class="highlight active">Email</a>
           <a href="../password" class="highlight nactive">Password</a>
-          <a href="../verify" class="highlight nactive">Verify</a>
           <a href="../discord" class="highlight nactive">Discord</a>
-          <a href="../privacy" class="highlight nactive">Privacy</a>
           <a href="../referrals" class="highlight nactive">Referrals</a>
         </div>
         <hr>
