68b5daeb7e149efbb822e7cfbfca331961ca45cf
Turan Ates <jamesdusky@dustytavern.com>
2021-06-25 20:10:34 -0400

    Debug

--- settings/email/format.php
+++ settings/email/format.php
@@ -112,8 +112,8 @@ session_start();
   <div class="container">
     <div class="header">
       <div class="mtHeader">
-        <img src="https://mobitracker.co/android-chrome-512x512.png" alt="">
-        <a class="mtTitle" href="https://mobitracker.co">MOBITRACKER</a>
+
+        <a class="mtTitle" href="https://mobitracker.co"><img src="https://mobitracker.co/android-chrome-512x512.png" alt="">MOBITRACKER</a>
       </div>
       <h1>Email Verification</h1>
     </div>
