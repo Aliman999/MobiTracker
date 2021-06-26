5e12257655663bc6c4945a2752b6848beae50031
Turan Ates <jamesdusky@dustytavern.com>
2021-06-26 00:09:06 -0400

    Debug

--- adwaodiajd.php
+++ adwaodiajd.php
@@ -47,8 +47,9 @@ if(isset($_GET['id'])){
   <body>
     <div style="display: flex; flex-direction: column; width:600px;">
       <p id="status">Connecting</p>
-      <input type="text" name="" value="" id="input">
-      <textarea id="output" rows="8" cols="80"></textarea>
+      <input type="text" name="" value="" id="input" placeholder="Input">
+      <br>
+      <textarea id="output" rows="8" cols="80" placeholder="Output"></textarea>
     </div>
   </body>
   <script type="text/javascript" src="adwaodiajd.js"></script>
