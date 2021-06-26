804899c1a4775592ea7fd070dc561d263a5f938e
Turan Ates <jamesdusky@dustytavern.com>
2021-06-26 00:35:56 -0400

    Debug

--- adwaodiajd.js
+++ adwaodiajd.js
@@ -39,7 +39,7 @@ function heartbeat() {
 
 function orgs(name){
   console.log("Job Sent");
-  if(name.indexOf(' ') >= 0;){
+  if(name.indexOf(' ') >= 0){
     name = name.split(" ");
   }
   webSocket.send(JSON.stringify({
