39d6da2be3161d4967406faed824c099385977c2
Turan Ates <jamesdusky@dustytavern.com>
2021-06-25 20:42:50 -0400

    Debug

--- settings/email/format.php
+++ settings/email/format.php
@@ -135,8 +135,6 @@ session_start();
         <tbody>
           <td>
             <a href="https://mobitracker.co/auth/email?token='.$_SESSION["jwt"].'" class="lButton highlight">Verify Email</a>
-          </td>
-          <td>
             <p>This is an automated email to verify your email. If it wasnt you please ignore this.<br><br>If it persists please join our discord for help. <a href="https://discord.com/invite/xT4YfcxmrA" class="rButton">Discord</a></p>
           </td>
         </tbody>
