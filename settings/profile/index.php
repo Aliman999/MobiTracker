f5fca6b02a6b60498536afd903fc4eb649c09127
Turan Ates <jamesdusky@dustytavern.com>
2021-06-27 15:45:35 -0400

    Debug

--- settings/profile/index.php
+++ settings/profile/index.php
@@ -89,7 +89,7 @@ if(empty($_SESSION['loggedin']) || $_SESSION['loggedin'] === false){
           <div class="faded">
             <p><span class="rBold"><?php echo $_SESSION['username'] ?></span><br><span id="displayName" class="subP"></span></p>
             <p><span class="rBold">Enlisted</span><br><span class="subP">June 24, 2013</span></p>
-            <p><span class="rBold">Badge:</span><br><span id="Badge" class="subP"><img class="badgeImg" src="https://robertsspaceindustries.com/media/cs5xmnc9byun2r/heap_thumb/Vice-Admiral.png">Vice Admiral</span></p>
+            <p><span class="rBold">Badge:</span><br><table><td><span id="Badge" class="subP"><img class="badgeImg" src="https://robertsspaceindustries.com/media/cs5xmnc9byun2r/heap_thumb/Vice-Admiral.png">Vice Admiral</span></td></table></p>
           </div>
           <div class="faded">
           </div>
