5862a829bcd1aaa8f68be07e3d81f1bddc9a3aa8
Turan Ates <jamesdusky@dustytavern.com>
2021-06-27 15:32:12 -0400

    Debug

--- settings/profile/main.js
+++ settings/profile/main.js
@@ -7,5 +7,6 @@ var display = setInterval(()=>{
 
 function init(){
   var field = document.getElementsByClassName("setting")[0];
-  
+  var faded = document.getElementsByClassName("faded")[0];
+  faded.style.opacity = 1;
 }
