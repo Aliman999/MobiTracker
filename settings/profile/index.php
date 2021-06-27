f8119a0ecc6f844dcbd2d03d60dd6b11e715081f
Turan Ates <jamesdusky@dustytavern.com>
2021-06-27 15:34:21 -0400

    Debug

--- settings/profile/index.php
+++ settings/profile/index.php
@@ -83,12 +83,14 @@ if(empty($_SESSION['loggedin']) || $_SESSION['loggedin'] === false){
         </div>
         <hr>
         <div class="setting">
-          <div>
+          <div id="loadingContainer">
             <p><span class="rBold">Loading</span></p><img src="../../src/loading.png" class="loading">
           </div>
-          <div class="faded">
-            <p><span class="rBold"><?php echo $_SESSION['username'] ?></span><br><span id="displayName" class="subP"></span></p>
-            <p><span class="rBold">Badge:</span><br><img class="badgeImg" src=""><span id="Badge" class="subP"></span></p>
+          <div class="">
+            <div class="faded">
+              <p><span class="rBold"><?php echo $_SESSION['username'] ?></span><br><span id="displayName" class="subP"></span></p>
+              <p><span class="rBold">Badge:</span><br><img class="badgeImg" src=""><span id="Badge" class="subP"></span></p>
+            </div>
           </div>
         </div>
       </div>
--- settings/profile/main.js
+++ settings/profile/main.js
@@ -6,7 +6,9 @@ var display = setInterval(()=>{
 }, 1000);
 
 function init(){
-  var field = document.getElementsByClassName("setting")[0];
+  var loading = document.getElementsById("loadingContainer");
+  loading.style.opacity = 0;
+  var field = document.getElementsByClassName("setting")[0].children[1];
   var faded = document.getElementsByClassName("faded")[0];
   faded.style.opacity = 1;
 }
