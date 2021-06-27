04794039a53f8848a24e8b313acf2cf1524f7f9c
Turan Ates <jamesdusky@dustytavern.com>
2021-06-27 15:19:25 -0400

    Debug

--- settings/profile/main.js
+++ settings/profile/main.js
@@ -1,5 +1,11 @@
-var load = setInterval(()=>{
+var display = setInterval(()=>{
   if(user){
+    init();
     clearInterval(load);
   }
 }, 1000);
+
+function init(){
+  var field = document.getElementsByClassName("setting")[0];
+
+}
