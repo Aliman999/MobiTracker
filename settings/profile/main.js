dc4146756c5840bae5ccad571a0627a1bf92e248
Turan Ates <jamesdusky@dustytavern.com>
2021-06-28 11:18:02 -0400

    Debug

--- settings/profile/main.js
+++ settings/profile/main.js
@@ -99,6 +99,44 @@ function init(){
 
   faded.meta.appendChild(enlistCont);
 
+  //Languages
+  if(profile.profile.fluency.length > 0){
+    const langCont = document.createElement("p");
+    boldSpan = document.createElement("span");
+    boldSpan.innerText = "Languages:";
+    boldSpan.className = "rBold";
+    subP = document.createElement("span");
+    subP.className = "subP";
+    subP.innerText = profile.profile.fluency.join(", ");
+
+
+    enlistCont.appendChild(boldSpan);
+    enlistCont.appendChild(document.createElement("br"));
+    enlistCont.appendChild(subP);
+
+    faded.meta.appendChild(enlistCont);
+  }
+
+  //Orgs
+  const enlistCont = document.createElement("p");
+  boldSpan = document.createElement("span");
+  boldSpan.innerText = "Enlisted:";
+  boldSpan.className = "rBold";
+  subP = document.createElement("span");
+  subP.className = "subP";
+
+  var d = new Date(profile.profile.enlisted);
+  subP.innerText = d.toLocaleString("en-US", {
+    month: "long",
+    day: "2-digit",
+    year: "numeric",
+  });
+  enlistCont.appendChild(boldSpan);
+  enlistCont.appendChild(document.createElement("br"));
+  enlistCont.appendChild(subP);
+
+  faded.meta.appendChild(enlistCont);
+
   var x = 0;
   var display = setInterval(()=>{
     faded[x]
