3f104326710665ae5110e16b3a2d6ce0f63993a6
Turan Ates <jamesdusky@dustytavern.com>
2021-07-01 16:18:33 -0400

    Debug

--- js/nav.js
+++ js/nav.js
@@ -79,7 +79,7 @@ function tcpp(){
   container.style.zIndex = "10";
 
   var disclaimer = document.createElement("p");
-  disclaimer.innerText = "Test";
+  disclaimer.innerText = "We've recently changed our Terms and Conditions and Privacy Policies after your account was created. You're required to agree to the Terms and Conditions and Privacy Policy to allow MobiTracker to provide the service it does. Thank you. -James";
 
   container.appendChild(disclaimer);
 
