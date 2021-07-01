47d284bba6ce5de9b9e3639010f8397b93016555
Turan Ates <jamesdusky@dustytavern.com>
2021-07-01 13:34:08 -0400

    Fixed register.php

--- src/register.php
+++ src/register.php
@@ -25,82 +25,60 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
   }else{
     // Prepare a select statement
     $sql = "SELECT id, username, signup FROM players WHERE (username = ? AND signup = 1)";
+    $param_username = trim($_POST["username"]);
+    // Attempt to execute the prepared statement
+    $result = mysqli_query($link, $sql);
+    $row = mysqli_fetch_assoc($result);
 
-    if($stmt = mysqli_prepare($link, $sql)){
-      // Bind variables to the prepared statement as parameters
-      mysqli_stmt_bind_param($stmt, "s", $param_username);
+    if(count($row)>0){
+      $username_err = "This RSI Handle has already been taken.";
+    }else{
+      $obj = json_decode(file_get_contents($requestURL.$_POST['username']), true);
+      $sql = "SELECT id, username, signup FROM players WHERE (username = $param_username AND signup = 0);";
 
-      // Set parameters
-      $param_username = trim($_POST["username"]);
-      // Attempt to execute the prepared statement
-      if(mysqli_stmt_execute($stmt)){
-        /* store result */
-        mysqli_stmt_store_result($stmt);
-
-        if(mysqli_stmt_num_rows($stmt) == 1){
-          $username_err = "This handle has been taken";
-        }else{
-          $obj = json_decode(file_get_contents($requestURL.$_POST['username']), true);
-          $sql = "SELECT id, username, signup FROM players WHERE (username = $param_username AND signup = 0);";
-
-          if($obj['data']['profile']){
-            $result = mysqli_query($link, $sql);
-            $emparray = array();
-            $row = mysqli_fetch_assoc($result);
-            if(count($row)>0){
-              $username_err = "This RSI Handle has already been taken.";
-            }
-          }else{
-            $username_err = "This is not a RSI Handle";
-          }
-
-          $res = $obj['data']['profile']['badge'];
-          if($res == "Developer" || $res == "Administrator" || $res == "Moderator" || $res == "Staff" || $res == "Creator"){
-            $username_err = "You cannot use this Handle";
-            $username = "";
-          }
-          $avatar = $obj['data']['profile']['image'];
-          $username = $obj['data']['profile']['handle'];
-
-          $sql = "SELECT cID FROM players WHERE cID = ?";
-
-
-          if($stmt = mysqli_prepare($link, $sql)){
-            mysqli_stmt_bind_param($stmt, "s", $cID);
-            $cID = $obj['data']['profile']['id'];
-            if($cID == "n/a"){
-              $cID = 0;
-            }else{
-              $cID = substr($cID, 1);
-            }
-
-            if(mysqli_stmt_execute($stmt)){
-
-              mysqli_stmt_store_result($stmt);
-
-              if(mysqli_stmt_num_rows($stmt) == 1){
-                $sql = "SELECT players WHERE cID = $cID AND signup = 1;";
-
-                if($result = mysqli_query($link, $sql)){
-                  $sql = "UPDATE players SET username = '$username' WHERE cID = $cID AND signup = 1;";
-
-                  $result = mysqli_query($link, $sql);
-                  $username_err = "You changed your username. <br>Login with your new username.";
-                }else{
-                  $update = 1;
-                }
-              }else{
-                $update = 0;
-              }
-            }
-          }
+      if($obj['data']['profile']){
+        $result = mysqli_query($link, $sql);
+        $row = mysqli_fetch_assoc($result);
+        if(count($row)>0){
+          $username_err = "This RSI Handle has already been taken.";
         }
-      } else{
-        echo "Oops! Something went wrong. Please try again later.";
+      }else{
+        $username_err = "This is not a RSI Handle";
       }
 
-      // Close statement
-      mysqli_stmt_close($stmt);
+      $res = $obj['data']['profile']['badge'];
+      if($res == "Developer" || $res == "Administrator" || $res == "Moderator" || $res == "Staff" || $res == "Creator"){
+        $username_err = "You cannot use this Handle";
+        $username = "";
+      }
+      $avatar = $obj['data']['profile']['image'];
+      $username = $obj['data']['profile']['handle'];
+
+      $cID = $obj['data']['profile']['id'];
+      if($cID == "n/a"){
+        $cID = 0;
+      }else{
+        $cID = substr($cID, 1);
+      }
+
+      $sql = "SELECT cID FROM players WHERE cID = ?";
+      if($result = mysqli_query($link, $sql)){
+        $row = mysqli_fetch_assoc($result)
+        if(count($row) > 0){
+          $sql = "SELECT players WHERE cID = $cID AND signup = 1;";
+
+          if($result = mysqli_query($link, $sql)){
+            $sql = "UPDATE players SET username = '$username' WHERE cID = $cID AND signup = 1;";
+
+            $result = mysqli_query($link, $sql);
+            $username_err = "You changed your username. <br>Login with your new username.";
+          }else{
+            $update = 1;
+          }
+        }else{
+          $update = 0;
+        }
+      }
     }
   }
 
@@ -116,22 +94,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
   // Validate confirm password
   if(empty(trim($_POST["email"]))){
       $email_err = "Please enter an Email";
-  }else{
-    $sql = "SELECT email FROM players WHERE (email = ?)";
-
-    if($stmt = mysqli_prepare($link, $sql)){
-      mysqli_stmt_bind_param($stmt, "s", $param_email);
-      $param_email = trim($_POST["email"]);
-
-      if(mysqli_stmt_execute($stmt)){
-        mysqli_stmt_store_result($stmt);
-        if(mysqli_stmt_num_rows($stmt) == 1){
-          $email_err = "This email has already been used";
-        }else{
-          $email = trim($_POST["email"]);
-        }
-      }
-    }
   }
 
   // Check input errors before inserting in database
@@ -144,29 +106,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
       $sql = "INSERT INTO players (cID, username, password, email, organization, avatar, signup) VALUES ($cID, '$param_username', '$param_password', '$param_email', '{}', '$param_avatar', 1);";
       echo $sql;
       mysqli_query($link, $sql);
-      // Close statement
-      mysqli_stmt_close($stmt);
   }
   if(empty($username_err) && empty($password_err) && empty($email_err) && $update == 1){
-          // Prepare an insert statement
-          $sql = "UPDATE players SET password = ?, email = ?, avatar = ?, signup = 1 WHERE username = ?";
-          if($stmt = mysqli_prepare($link, $sql)){
-              // Bind variables to the prepared statement as parameters
-              mysqli_stmt_bind_param($stmt, "ssss",  $param_password, $param_email, $param_avatar, $param_username);
-
-              // Set parameters
-              $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-              $param_email = $email;
-              $param_avatar = $avatar;
-              $param_username = $username;
-
-              // Attempt to execute the prepared statement
-              mysqli_stmt_execute($stmt);
-
-              // Close statement
-              mysqli_stmt_close($stmt);
-          }
-      }
+  
+    $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+    $sql = "UPDATE players SET password = $param_password, email = $email, avatar = $avatar, signup = 1 WHERE username = $username";
+    mysqli_query($link, $sql);
+  }
 
   // Close connection
   mysqli_close($link);
