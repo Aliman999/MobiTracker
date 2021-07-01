d678b0e1b2004460e2b7f886821b9b5871c44361
Turan Ates <jamesdusky@dustytavern.com>
2021-07-01 16:26:30 -0400

    Debug

--- js/nav.js
+++ js/nav.js
@@ -79,7 +79,7 @@ function tcpp(){
   container.style.zIndex = "10";
 
   var disclaimer = document.createElement("p");
-  disclaimer.innerHTML = "<p>We've recently changed our Terms and Conditions and Privacy Policies after your account was created. You're required to agree to the Terms and Conditions and Privacy Policy to allow MobiTracker to provide the service it does. Thank you. -James</p><br><p><a href='https://mobitracker.co/terms' class='highlight'>Terms and Conditions</a><br><a href='https://mobitracker.co/privacy' class='highlight'>Privacy Policy</a></p><div style='display:flex; justify-content:center;'><a class='rButton highlight-green' style='margin-right:8px;'>Agree</a><a class='rButton highlight-red'>Disagree</a></div>";
+  disclaimer.innerHTML = "<p>We've recently changed our Terms and Conditions and Privacy Policies after your account was created. You're required to agree to the Terms and Conditions and Privacy Policy to allow MobiTracker to provide the service it does. Thank you. -James</p><br><p><a href='https://mobitracker.co/terms' class='highlight'>Terms and Conditions</a><br><a href='https://mobitracker.co/privacy' class='highlight'>Privacy Policy</a></p><div style='display:flex; justify-content:center;'><a class='rButton highlightSoftGreen' style='margin-right:8px;'>Agree</a><a class='rButton highlight-red'>Disagree</a></div>";
 
   container.appendChild(disclaimer);
 
