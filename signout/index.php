525ce2fc13361dbffdb38443444300ec0c171b7c
Turan Ates <jamesdusky@dustytavern.com>
2021-07-01 17:11:57 -0400

    Debug

--- signout/index.php
+++ signout/index.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 session_start();
-if(dirname($_SERVER['REQUEST_URI']) != "beta"){
+if(dirname($_SERVER['REQUEST_URI']) != "beta" || dirname($_SERVER['REQUEST_URI']) != "/"){
   $ref = dirname($_SERVER['REQUEST_URI']);
 }else{
   $ref = "";
