7cd608558d8c5b56dd9fe798255b81f065e9db84
Turan Ates <jamesdusky@dustytavern.com>
2021-07-03 18:36:47 -0400

    Debug

--- js/main.js
+++ js/main.js
@@ -578,7 +578,7 @@ function clearBox(elementID){
   elementID.style.border = "";
   elementID.style.marginTop = "";
 }
-
+var ready = false;
 function populateHeader(jsonObj){
   retries = 0;
   if(dataCount > 0){
@@ -1225,6 +1225,7 @@ function populateHeader(jsonObj){
   var display = setInterval(()=>{
     faded[x].style.opacity = 1;
     if(x == faded.length-1){
+      ready = true;
       clearInterval(display);
     }else{
       x++;
@@ -1532,11 +1533,13 @@ function showReview(){
   }
   var x = 0;
   var display = setInterval(()=>{
-    playerReview[x].style.opacity = 1;
-    if(x == playerReview.length-1){
-      clearInterval(display);
-    }else{
-      x++;
+    if(ready){
+      playerReview[x].style.opacity = 1;
+      if(x == playerReview.length-1){
+        clearInterval(display);
+      }else{
+        x++;
+      }
     }
   }, 250);
 }
