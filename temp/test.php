94cc5f0dc8cbb04d7a5913035e6f1b1e5a6d5e26
Turan Ates <jamesdusky@dustytavern.com>
2021-07-08 15:13:04 -0400

    Debug

--- temp/test.php
+++ temp/test.php
@@ -4,7 +4,10 @@ function make_pipeline(...$funcs)
 {
     return function($arg) use ($funcs)
     {
-        return -1;
+        foreach ($funcs as $function) {
+          $arg = $func($arg);
+        }
+        return $arg;
     };
 }
 
@@ -12,5 +15,3 @@ $fun = make_pipeline( function($x) { return $x * 3; },
                       function($x) { return $x + 1; },
                       function($x) { return $x / 2; } );
 echo $fun(3); # should print 5
-
-//
