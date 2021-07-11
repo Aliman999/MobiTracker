53fa33d561e946322fb6c34a51baa810fd075f36
Turan Ates <jamesdusky@dustytavern.com>
2021-07-11 03:52:27 -0400

    Debug

--- gtemps/nav.php
+++ gtemps/nav.php
@@ -22,12 +22,11 @@ if(strpos($base, "?option") !== false){
   </div>
   <div class="eleContainer">
     <a href="<?php if($base != ""){echo "https://mobitracker.co/";} ?>" class="navEle <?php if($base == ""){echo "navActive";} ?>"><img src="https://mobitracker.co/src/repHub.png" class="menuIcon"/><p>Reputation Hub</p></a>
+    <a href="<?php if($base != "contracts"){echo "";}?>" class="navEle <?php if($base == ""){echo "navActive";} ?>"><img src="https://mobitracker.co/src/contracts.png" class="menuIcon"/><p>Information Hub</p></a>
     <a href="<?php if($base != "contracts"){echo "https://mobitracker.co/contracts";}?>" class="navEle <?php if($base == "contracts"){echo "navActive";} ?>"><img src="https://mobitracker.co/src/contracts.png" class="menuIcon"/><p>Contracts</p></a>
-    <a href="<?php if($base != "contracts"){echo "https://mobitracker.co/contracts";}?>" class="navEle <?php if($base == "contracts"){echo "navActive";} ?>"><img src="https://mobitracker.co/src/contracts.png" class="menuIcon"/><p>Player History</p></a>
-    <a href="<?php if($base != "contracts"){echo "https://mobitracker.co/contracts";}?>" class="navEle <?php if($base == "contracts"){echo "navActive";} ?>"><img src="https://mobitracker.co/src/contracts.png" class="menuIcon"/><p>Organization History</p></a>
     <a href="<?php if($base != "discord"){echo "https://mobitracker.co/discord";}?>" class="navEle <?php if($base == "discord"){echo "navActive";} ?>"><img src="https://mobitracker.co/src/discord.png" class="menuIcon"/><p>Discord</p></a>
     <a href="<?php if($base != "faq"){echo "https://mobitracker.co/faq";}?>" class="navEle <?php if($base == "faq"){echo "navActive";} ?>"><img src="https://mobitracker.co/src/faq.png" class="menuIcon"/><p>FAQ</p></a>
-    <a href="https://finder.deepspacecrew.com/" class="navEle" target="_blank"><img src="https://mobitracker.co/beta/src/UIF_mtco.png" class="menuIcon"/><p>Universal Item Finder</p></a>
+    <a href="https://finder.cornerstonebase.space/" class="navEle" target="_blank"><img src="https://mobitracker.co/beta/src/UIF_mtco.png" class="menuIcon"/><p>Universal Item Finder</p></a>
     <a href="https://patreon.com/mobitracker" class="navEle" target="_blank"><img src="https://mobitracker.co/src/patreon.png" class="menuIcon"/><p>Patreon</p></a>
   </div>
 </div>
