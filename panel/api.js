73cd29ea3c8965dacedab119b8a3b575de7c810b
Turan Ates <jamesdusky@dustytavern.com>
2021-07-16 14:51:16 -0400

    Debug

--- panel/api.js
+++ panel/api.js
@@ -1,3 +1,5 @@
+const _ = require("angular-loading-bar");
+
 var tokenHeader = document.getElementsByName("token")[0];
 var jwt = document.getElementsByName("jwt")[0];
 var webSocket = null;
@@ -5,88 +7,85 @@ var bool = true;
 
 socket();
 
-var players = new ldBar(".players", {
-  "preset":'fan',
-  "img": '',
-  "fill-dir": 'btt',
-  "fill": '#25b',
-  "fill-background": '#ddd',
-  "fill-background-extrude": 3,
-  "pattern-size": null,
-  "stroke-dir": 'normal',
-  "stroke-width": '3',
-  "stroke-trail": '#ddd',
-  "stroke-trail-width": 0.5,
-  "duration": 1,
-  "easing": 'linear',
-  "value": 0,
-  "img-size": null,
-  "set-dim": true,
-  "aspect-ratio": "xMidYMid",
-  "transition-in": false,
-  "min": 0,
-  "max": 100,
-  "precision": 0,
-  "padding": undefined
-});
-var crawler = new ldBar(".crawler", {
-  "preset": 'fan',
-  "img": '',
-  "fill-dir": 'btt',
-  "fill": '#25b',
-  "fill-background": '#ddd',
-  "fill-background-extrude": 3,
-  "pattern-size": null,
-  "stroke-dir": 'normal',
-  "stroke-width": '3',
-  "stroke-trail": '#ddd',
-  "stroke-trail-width": 0.5,
-  "duration": 1,
-  "easing": 'linear',
-  "value": 0,
-  "img-size": null,
-  "set-dim": true,
-  "aspect-ratio": "xMidYMid",
-  "transition-in": false,
-  "min": 0,
-  "max": 100,
-  "precision": 0,
-  "padding": undefined
-});
-var scanner = new ldBar(".scanner", {
-  "preset": 'fan',
-  "img": '',
-  "fill-dir": 'btt',
-  "fill": '#25b',
-  "fill-background": '#ddd',
-  "fill-background-extrude": 3,
-  "pattern-size": null,
-  "stroke-dir": 'normal',
-  "stroke-width": '3',
-  "stroke-trail": '#ddd',
-  "stroke-trail-width": 0.5,
-  "duration": 1,
-  "easing": 'linear',
-  "value": 0,
-  "img-size": null,
-  "set-dim": true,
-  "aspect-ratio": "xMidYMid",
-  "transition-in": false,
-  "min": 0,
-  "max": 100,
-  "precision": 0,
-  "padding": undefined
-});
+var bars = [
+  new ldBar(".players", {
+    "preset": 'fan',
+    "img": '',
+    "fill-dir": 'btt',
+    "fill": '#25b',
+    "fill-background": '#ddd',
+    "fill-background-extrude": 3,
+    "pattern-size": null,
+    "stroke-dir": 'normal',
+    "stroke-width": '3',
+    "stroke-trail": '#ddd',
+    "stroke-trail-width": 0.5,
+    "duration": 1,
+    "easing": 'linear',
+    "value": 0,
+    "img-size": null,
+    "set-dim": true,
+    "aspect-ratio": "xMidYMid",
+    "transition-in": false,
+    "min": 0,
+    "max": 100,
+    "precision": 0,
+    "padding": undefined
+  }),
+  new ldBar(".crawler", {
+    "preset": 'fan',
+    "img": '',
+    "fill-dir": 'btt',
+    "fill": '#25b',
+    "fill-background": '#ddd',
+    "fill-background-extrude": 3,
+    "pattern-size": null,
+    "stroke-dir": 'normal',
+    "stroke-width": '3',
+    "stroke-trail": '#ddd',
+    "stroke-trail-width": 0.5,
+    "duration": 1,
+    "easing": 'linear',
+    "value": 0,
+    "img-size": null,
+    "set-dim": true,
+    "aspect-ratio": "xMidYMid",
+    "transition-in": false,
+    "min": 0,
+    "max": 100,
+    "precision": 0,
+    "padding": undefined
+  }),
+  new ldBar(".scanner", {
+    "preset": 'fan',
+    "img": '',
+    "fill-dir": 'btt',
+    "fill": '#25b',
+    "fill-background": '#ddd',
+    "fill-background-extrude": 3,
+    "pattern-size": null,
+    "stroke-dir": 'normal',
+    "stroke-width": '3',
+    "stroke-trail": '#ddd',
+    "stroke-trail-width": 0.5,
+    "duration": 1,
+    "easing": 'linear',
+    "value": 0,
+    "img-size": null,
+    "set-dim": true,
+    "aspect-ratio": "xMidYMid",
+    "transition-in": false,
+    "min": 0,
+    "max": 100,
+    "precision": 0,
+    "padding": undefined
+  })
+];
 
 function display(response){
-  console.log(myBar);
-  /*
-  var bar = document.getElementsByClassName("mkCharts");
-
-  bar[0].dataset.value = Math.round((response.data.player.current / response.data.player.max * 100));
-  bar[1].dataset.value = Math.round((response.data.crawler.current / response.data.crawler.max * 100));
-  bar[2].dataset.value = Math.round((response.data.scanner.current / response.data.scanner.max * 100))
-  */
+  bars[0].set(Math.round((response.data.player.current / response.data.player.max * 100)));
+  bars[1].set(Math.round((response.data.crawler.current / response.data.crawler.max * 100)));
+  bars[2].set(Math.round((response.data.scanner.current / response.data.scanner.max * 100)));
 }
 
 function socket(){
