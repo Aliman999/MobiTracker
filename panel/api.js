1846e2c721d698736100adefb81d48224deac4a3
Turan Ates <jamesdusky@dustytavern.com>
2021-07-16 10:50:07 -0400

    Debug

---
+++ panel/api.js
@@ -0,0 +1,59 @@
+var tokenHeader = document.getElementsByName("token")[0];
+var jwt = document.getElementsByName("jwt")[0];
+var webSocket = null;
+var profile;
+
+var waitUser = setInterval(()=>{
+  if(user){
+    socket();
+    clearInterval(waitUser);
+  }
+}, 1000);
+
+function socket(){
+  webSocket = new WebSocket("wss://ws.mobitracker.co:2599");
+  webSocket.onopen = function(){
+    message = {
+      type:"internal",
+      token:jwt.content
+    };
+    webSocket.send(JSON.stringify(message));
+    console.log("Authentication Sent");
+    heartbeat();
+  }
+  webSocket.onmessage = function(event){
+    console.log("Authentication Response");
+    var response = JSON.parse(event.data);
+    if(response.type == "authentication"){
+      if(!profile){
+        api(user.sessionUser);
+      }
+    }else if (response.type == "response") {
+      profile = response.data;
+      console.log(response);
+    }
+  }
+  webSocket.onerror = function(err){
+    console.log("Error");
+    setTimeout(socket, 3000);
+  }
+  webSocket.onclose = function(){
+    console.log("Connection Closed");
+    setTimeout(socket, 3000);
+  };
+}
+
+function heartbeat() {
+  if (!webSocket) return;
+  if (webSocket.readyState !== 1) return;
+  webSocket.send(JSON.stringify({type:"ping"}));
+  setTimeout(heartbeat, 3000);
+}
+
+function api(name){
+  console.log("Job Sent");
+  webSocket.send(JSON.stringify({
+    type:"job",
+    token:name
+  }));
+}
---
+++ panel/index.php
@@ -0,0 +1,83 @@
+<?php
+// Initialize the session
+session_start();
+if (empty($_SESSION['token'])) {
+  $_SESSION['token'] = bin2hex(random_bytes(32));
+}
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>MobiTracker</title>
+    <link href="https://fonts.googleapis.com/css2?family=Exo:wght@300;400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css?v=2.0">
+    <link rel="stylesheet" href="../css/patreon.css">
+    <link rel="stylesheet" href="../css/nav.css">
+    <link rel="stylesheet" href="locale.css">
+    <link rel="stylesheet" href="css/mk_charts.css" />
+    <link rel="apple-touch-icon" sizes="180x180" href="https://mobitracker.co/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://mobitracker.co/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://mobitracker.co/favicon-16x16.png">
+    <link rel="manifest" href="https://mobitracker.co/site.webmanifest">
+    <link rel="mask-icon" href="https://mobitracker.co/safari-pinned-tab.svg" color="#5bbad5">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="MobiTracker.co" />
+    <meta property="og:description" content="Find who to trust when you need a hand to lead your armed escort or who to avoid letting on your ship" />
+    <meta property="og:url" content="https://mobitracker.co" />
+    <meta property="og:image" content="https://mobitracker.co/android-chrome-512x512.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#253139">
+    <?php
+    echo "<meta name='token' content=".$_SESSION['token'].">";
+    ?>
+    <?php
+    if($_SESSION["loggedin"]){
+      echo "<meta name='jwt' content=".$_SESSION['jwt'].">";
+    }
+    ?>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-177343701-1"></script>
+    <script type="text/javascript" src="../js/ga.js"></script>
+    <script src="js/mk_charts.js"></script>
+  </head>
+
+  <body>
+    <?php include "../gtemps/nav.php"; ?>
+    <div id="canvas">
+      <div class="headerContainer">
+        <div id="login_status" class="login_menu">
+          <a><img id="mtLogo" src="../android-chrome-512x512.png" class="mtLogo"></a>
+          <a id="mtTitle" class="mtTitle">MOBITRACKER</a>
+          <?php
+          if(!isset($_SESSION["loggedin"])){
+            echo "<a id='lmLBtn' class='verify_btn lmBtn'>Login</a>";
+            echo "<a id='lmSBtn' class='verify_btn'>Sign Up</a>";
+          }
+          if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+            echo "<div class='userContainer'><img class='verified' src='../src/verified.png'><p class='loginName'>".$_SESSION['username']."</p></div>";
+          }
+          ?>
+          <div id="navBtnContainer" class="navBtnContainer">
+            <div class="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "loggedIn";}else{echo "loggedOut";} ?>"></div>
+            <div class="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "loggedIn";}else{echo "loggedOut";} ?>"></div>
+            <div class="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "loggedIn";}else{echo "loggedOut";} ?>"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="titleContainer">
+        <p class="webTitle">Essential to your Organization!</p>
+        <p class="webSlogan">Easily access StarCitizen's player Reputation Hub through discord!</p>
+      </div>
+      <div class="container-header">
+        <div class="container">
+          <div class="mkCharts" data-percent="35"></div>
+        </div>
+      </div>
+    </div>
+    <?php include "../gtemps/footer.php"; ?>
+    <script type="text/javascript" src="../js/api.js"></script>
+    <script type="text/javascript" src="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "../js/nav.js";}else{echo "../js/nSession.js";} ?>" async></script>
+  </body>
+</html>
