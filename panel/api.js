26372193d77a649685d782de805a439ca96ebaf5
Turan Ates <jamesdusky@dustytavern.com>
2021-07-16 14:38:46 -0400

    Debug

--- panel/api.js
+++ panel/api.js
@@ -6,12 +6,41 @@ var bool = true;
 socket();
 
 function display(response){
+  var myBar = new ldBar(".myContainer", {
+    "type": 'stroke',
+    "img": '',
+    "path": 'M10 10L90 10M90 8M90 12',
+    "fill-dir": 'btt',
+    "fill": '#25b',
+    "fill-background": '#ddd',
+    "fill-background-extrude": 3,
+    "pattern-size": null,
+    "stroke-dir": 'normal',
+    "stroke": '#25b',
+    "stroke-width": '3',
+    "stroke-trail": '#ddd',
+    "stroke-trail-width": 0.5,
+    "duration": 1,
+    "easing": 'linear',
+    "value": 0,
+    "img-size": null,
+    "bbox": null,
+    "set-dim": true,
+    "aspect-ratio": "xMidYMid",
+    "transition-in": false,
+    "min": 0,
+    "max": 100,
+    "precision": 0,
+    "padding": undefined
+  });
+  console.log(myBar);
+  /*
   var bar = document.getElementsByClassName("mkCharts");
 
   bar[0].dataset.value = Math.round((response.data.player.current / response.data.player.max * 100));
   bar[1].dataset.value = Math.round((response.data.crawler.current / response.data.crawler.max * 100));
   bar[2].dataset.value = Math.round((response.data.scanner.current / response.data.scanner.max * 100))
-  run();
+  */
 }
 
 function socket(){
