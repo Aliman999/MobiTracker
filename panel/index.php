9d95c543c9096e7970d3d5ed37e96fc80e094139
Turan Ates <jamesdusky@dustytavern.com>
2021-07-16 11:09:45 -0400

    Debug

--- panel/index.php
+++ panel/index.php
@@ -88,7 +88,7 @@ if (empty($_SESSION['token'])) {
     </div>
     <?php include "../gtemps/footer.php"; ?>
     <script type="text/javascript" src="mk_charts.js"></script>
-    <script type="text/javascript" src="../js/api.js"></script>
+    <script type="text/javascript" src="api.js"></script>
     <script type="text/javascript" src="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "../js/nav.js";}else{echo "../js/nSession.js";} ?>" async></script>
   </body>
 </html>
