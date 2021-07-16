aa2e0b7c8fc819202788e5f2aa9533e573cf950b
Turan Ates <jamesdusky@dustytavern.com>
2021-07-16 14:24:45 -0400

    Debug

--- panel/index.php
+++ panel/index.php
@@ -77,7 +77,7 @@ if (empty($_SESSION['token'])) {
           <div id="loadingContainer">
             <p><span class="rBold">Loading</span></p><img src="../../src/loading.png" class="loading">
           </div>
-          <div class="ldBar" data-value="60" data-preset="fan"></div>
+          <div class="ldBar label-center" data-value="60" data-preset="fan" ></div>
         </div>
       </div>
     </div>
