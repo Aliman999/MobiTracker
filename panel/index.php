0468a1e2ff09e102e85c546a98b3d60281973131
Turan Ates <jamesdusky@dustytavern.com>
2021-07-16 11:21:02 -0400

    Debug

--- panel/index.php
+++ panel/index.php
@@ -70,19 +70,19 @@ if (empty($_SESSION['token'])) {
         <p class="webSlogan"></p>
       </div>
       <div class="container-header">
-        <div class="centerCont hidden">
+        <div class="centerCont">
           <div id="loadingContainer">
             <p><span class="rBold">Loading</span></p><img src="../../src/loading.png" class="loading">
           </div>
-          <div>
+          <div class="hidden">
             <div class="mkCharts" data-percent="0" data-color="#91A6FF" data-size="100" data-stroke="3"></div>
             <p>Player Scanner</p>
           </div>
-          <div>
+          <div class="hidden">
             <div class="mkCharts" data-percent="0" data-color="#78FFD6" data-size="100" data-stroke="3"></div>
             <p>Org Crawler</p>
           </div>
-          <div>
+          <div class="hidden">
             <div class="mkCharts" data-percent="0" data-color="#EB9486" data-size="100" data-stroke="3"></div>
             <p>Org Scanner</p>
           </div>
