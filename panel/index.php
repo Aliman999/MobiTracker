440d5da25e4c4037630c9e92e27a168c2eeb91ce
Turan Ates <jamesdusky@dustytavern.com>
2021-07-16 11:55:00 -0400

    Debug

--- panel/index.php
+++ panel/index.php
@@ -73,19 +73,7 @@ if (empty($_SESSION['token'])) {
         <div class="centerCont">
           <div id="loadingContainer">
             <p><span class="rBold">Loading</span></p><img src="../../src/loading.png" class="loading">
-          </div>  
-            <div class="faded">
-              <div class="mkCharts" data-percent="0" data-color="#91A6FF" data-size="100" data-stroke="3"></div>
-              <p>Player Scanner</p>
-            </div>
-            <div class="faded">
-              <div class="mkCharts" data-percent="0" data-color="#78FFD6" data-size="100" data-stroke="3"></div>
-              <p>Org Crawler</p>
-            </div>
-            <div class="faded">
-              <div class="mkCharts" data-percent="0" data-color="#EB9486" data-size="100" data-stroke="3"></div>
-              <p>Org Scanner</p>
-            </div>
+          </div>
         </div>
       </div>
     </div>
