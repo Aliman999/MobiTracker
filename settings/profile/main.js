aac5fb4f237716d804cfdff4361079d22e800c15
Turan Ates <jamesdusky@dustytavern.com>
2021-07-17 12:43:27 -0400

    Debug

--- settings/profile/main.js
+++ settings/profile/main.js
@@ -35,7 +35,7 @@ function init(){
     const verifyBtn = document.createElement("a");
     verifyBtn.innerText = "Verify";
     verifyBtn.className = "verify_btn";
-    faded.verify.appendChild(verifyImg);
+    verifyBtn.appendChild(verifyImg);
     faded.verify.appendChild(verifyBtn);
   }
 
