14b923ec3a84ba3ff05d42705d9edf95a455e80a
Turan Ates <jamesdusky@dustytavern.com>
2021-07-17 18:12:11 -0400

    Debug

--- settings/profile/main.js
+++ settings/profile/main.js
@@ -41,8 +41,8 @@ function verify(){
         query.open("GET", "https://mobitracker.co/beta/src/verify.php");
         query.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         query.setRequestHeader(tokenHeader.name, tokenHeader.content);
-        query.send();
         query.onload = null;
+        query.send();
       }
       init();
     }
