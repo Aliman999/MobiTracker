ad478fade2a993f4a64b0465deb9597ec2a58811
Turan Ates <jamesdusky@dustytavern.com>
2021-07-17 19:29:03 -0400

    Debug

--- settings/security/index.php
+++ settings/security/index.php
@@ -91,8 +91,8 @@ if(empty($_SESSION['loggedin']) || $_SESSION['loggedin'] === false){
       </div>
     </div>
     <?php include "../../gtemps/footer.php"; ?>
+    <script type="text/javascript" src="../../js/socket.js"></script>
     <script type="text/javascript" src="main.js"></script>
-    <script type="text/javascript" src="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "../../js/nav.js";}else{echo "../../js/nSession.js";} ?>"></script>
-    <script type="text/javascript" src="../../js/api.js"></script>
+    <script type="text/javascript" src="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "../../js/nav.js";}else{echo "../../js/nSession.js";} ?>" async></script>
   </body>
 </html>
