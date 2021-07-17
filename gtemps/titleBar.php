7516698e54330044917b7b5c4633bca269863a27
Turan Ates <jamesdusky@dustytavern.com>
2021-07-17 19:06:08 -0400

    Debug

--- gtemps/titleBar.php
+++ gtemps/titleBar.php
@@ -6,7 +6,7 @@ echo "<a id='lmSBtn' class='verify_btn'>Sign Up</a>";
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
 echo "<div class='userContainer'>";
 if($_SESSION['verified'] == 1){
-    echo "<img class='verified' src='../src/verified.png'>";
+    echo "<img class='verified' src='https://mobitracker.co/src/verified.png'>";
 }
 echo "<p class='loginName'>".$_SESSION['username']."</p></div>";
 }
