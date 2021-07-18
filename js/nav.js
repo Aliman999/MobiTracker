a2912fbcb0be769d328893766eec54a9d987a4ff
Turan Ates <jamesdusky@dustytavern.com>
2021-07-18 16:55:42 -0400

    Debug

--- js/nav.js
+++ js/nav.js
@@ -122,7 +122,7 @@ function tcpp(){
   clickCont.appendChild(agree);
   clickCont.appendChild(disagree);
 
-  disclaimer.innerHTML = "<p>We've recently changed our Terms and Conditions and Privacy Policies after your account was created. You're required to agree to the Terms and Conditions and Privacy Policy to allow MobiTracker to provide the service it does. Thank you. -James</p><br><div style='text-align:center; display:flex;'><a href='https://mobitracker.co/terms' class='highlight' style='margin:8px 0;'>Terms and Conditions</a><br><a href='https://mobitracker.co/privacy' class='highlight' style='margin:8px 0;'>Privacy Policy</a></div>";
+  disclaimer.innerHTML = "<p>We've recently changed our Terms and Conditions and Privacy Policies after your account was created. You're required to agree to the Terms and Conditions and Privacy Policy to allow MobiTracker to provide the service it does. Thank you. -James</p><br><div style='text-align:center; display:flex; flex-direction:column; justify-content:center;'><a href='https://mobitracker.co/terms' class='highlight' style='margin:8px 0;'>Terms and Conditions</a><br><a href='https://mobitracker.co/privacy' class='highlight' style='margin:8px 0;'>Privacy Policy</a></div>";
   disclaimer.appendChild(clickCont);
 
   container.appendChild(disclaimer);
