74fa6863059bcf7c07fe5c2594fcde461eab990e
Turan Ates <jamesdusky@dustytavern.com>
2021-07-18 12:07:43 -0400

    Debug

--- src/whitelist.php
+++ src/whitelist.php
@@ -3,10 +3,6 @@ if ($headers !== $_SESSION['token']) {
   unset($_SESSION['token']);
   exit(json_encode(['error' => 'Wrong token.']));
 }else{
-  $staff = array(
-    "JamesDusky",
-    "Kindmiss",
-    "Bo-Fone"
-  )
+  $staff = ["JamesDusky", "Kindmiss", "Bo-Fone"];
 }
 ?>
