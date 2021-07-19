5247bc1aa5063782de396195b059cfb7a086755d
Turan Ates <jamesdusky@dustytavern.com>
2021-07-19 04:53:24 -0400

    Debug

--- src/link.php
+++ src/link.php
@@ -8,21 +8,7 @@ define('DB_USERNAME', 'login');
 define('DB_PASSWORD', 'Ninjaman');
 define('DB_NAME', 'users');
 
-$keyType = "Main";
-
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-function getKey(){
-  global $id, $key, $count, $link;
-  $sql = "SELECT id, apiKey FROM apiKeys WHERE note like '%main%' GROUP BY id, apiKey, count ORDER BY count desc LIMIT 1;";
-  $result = mysqli_query($link, $sql);
-  $key = mysqli_fetch_assoc($result);
-  $id = $key['id'];
-  $key = $key['apiKey'];
-  $sql = "UPDATE apiKeys SET count = count-1 WHERE id = $id";
-  $result = mysqli_query($link, $sql);
-  return $key;
-}
 
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
@@ -40,6 +26,8 @@ if(isset($headers)){
     if(mysqli_query($link, $sql)){
       echo "Success";
     }else{
+      echo "Failed";
+      echo "\n".$sql;
       mysqli_error($link);
     }
   }else{
