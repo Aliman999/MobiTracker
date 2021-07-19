edd76382363c318f1070cf32da9a4fc02a8a5521
Turan Ates <jamesdusky@dustytavern.com>
2021-07-19 14:08:38 -0400

    Debug

--- settings/discord/index.php
+++ settings/discord/index.php
@@ -79,8 +79,8 @@
           <div>
             <p><span class="rBold">Discord:</span><br><span class="subP<?php if($row['discUser']){echo ' highlight-green';}else{echo ' highlight-red';}  ?>"><?php if($row['discUser']){echo 'LINKED';}else{echo 'NOT LINKED';} ?></span></p>
             <?php
-            if($row['dusername']){
-              echo '<a class="rButton highlight-red rightSide">Unlink</a>';
+            if(!$row['dusername']){
+              echo '<a class="rButton highlight-green rightSide">Link</a>';
             }
             ?>
           </div>
