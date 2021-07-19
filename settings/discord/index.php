c75369fac9fbc42b9d1f138090a87c7b066ef739
Turan Ates <jamesdusky@dustytavern.com>
2021-07-19 00:00:15 -0400

    Debug

--- settings/discord/index.php
+++ settings/discord/index.php
@@ -91,6 +91,7 @@
       </div>
     </div>
     <?php include "../../gtemps/footer.php"; ?>
+    <script type="text/javascript" src="main.js"></script>
     <script type="text/javascript" src="../../js/socket.js"></script>
     <script type="text/javascript" src="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "../../js/nav.js";}else{echo "../../js/nSession.js";} ?>" async></script>
   </body>
