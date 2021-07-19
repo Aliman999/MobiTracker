ff9f1d52dac76da2e7e614b518bfde2179d57800
Turan Ates <jamesdusky@dustytavern.com>
2021-07-18 21:53:48 -0400

    Debug

--- discord/oauth/oauth.js
+++ discord/oauth/oauth.js
@@ -2,6 +2,10 @@ window.onload = () => {
   const fragment = new URLSearchParams(window.location.hash.slice(1));
   const [accessToken, tokenType] = [fragment.get('access_token'), fragment.get('token_type')];
 
+  if (!accessToken) {
+    return document.getElementById('login').style.display = 'block';
+  }
+
   fetch('https://discord.com/api/users/@me', {
     headers: {
       authorization: `${tokenType} ${accessToken}`,
