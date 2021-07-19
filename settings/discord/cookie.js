398bf7c48217a5f0d447f1a4d054e3deb36e62e8
Turan Ates <jamesdusky@dustytavern.com>
2021-07-19 04:01:06 -0400

    Debug

--- settings/discord/cookie.js
+++ settings/discord/cookie.js
@@ -1,3 +1,4 @@
 var username = document.getElementsByName("username")[0];
 var id = document.getElementsByName("id")[0];
-document.cookie = "username="+username.content+"; cid="+id.content+";";
+document.cookie = "username="+username.content+";";
+document.cookie = "cid=" + id.content + ";""
