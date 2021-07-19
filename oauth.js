65e0f22f805a65725c5b321f1c85195d311de800
Turan Ates <jamesdusky@dustytavern.com>
2021-07-18 22:45:56 -0400

    Debug

--- oauth.js
+++ oauth.js
@@ -1,19 +1,22 @@
 window.onload = () => {
   const fragment = new URLSearchParams(window.location.hash.slice(1));
   const accessToken = fragment.get('access_token');
-
-  fetch("https://discordapp.com/api/oauth2/token", {
-    method: 'post',
-    body: JSON.stringify(body),
-    headers: { 
-      'grant_type': 'identify',
-      'code':accessToken,
-      'Access-Control-Allow-Origin':'*',
-      'Access-Control-Allow-Credentials':'true',
-      'Access-Control-Allow-Methods':'GET,HEAD,OPTIONS,POST,PUT',
-      'Access-Control-Allow-Headers':'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    },
-  })
+  const tokenUrl = "https://discordapp.com/api/oauth2/token";
+  const UserUrl = "https://discordapp.com/api/users/@me";
+  data = {
+    'client_id': '751252617451143219',
+    'client_secret': 'a7p2OQShjYQApq99f9zv2rjsz2_6Dg1Q',
+    'grant_type': 'authorization_code',
+    'code': code,
+    'redirect_uri': 'https://mobitracker.co/beta/',
+    'scope': 'identify'
+  }
+  header = {
+    'method': 'post',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'payload': data
+  }
+  fetch("https://discordapp.com/api/oauth2/token", header)
   .then(res => res.json())
   .then(json => console.log(json))
 };
