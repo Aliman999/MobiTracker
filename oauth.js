98f77713b25f30402f754355ee2bedacbe06afbb
Turan Ates <jamesdusky@dustytavern.com>
2021-07-18 22:36:23 -0400

    Debug

--- oauth.js
+++ oauth.js
@@ -1,11 +1,15 @@
 window.onload = () => {
   const fragment = new URLSearchParams(window.location.hash.slice(1));
   const accessToken = fragment.get('access_token');
-  
+
   fetch("https://discordapp.com/api/oauth2/token?grant_type=identify&code=", {
     method: 'post',
     body: JSON.stringify(body),
-    headers: { 'grant_type': 'identify', 'code':accessToken },
+    headers: { 
+      'grant_type': 'identify',
+      'code':accessToken,
+      'Access-Control-Allow-Headers'
+    },
   })
   .then(res => res.json())
   .then(json => console.log(json))
