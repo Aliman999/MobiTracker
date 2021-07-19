a123efbe15c3083767231695bfe197ca587ec5d0
Turan Ates <jamesdusky@dustytavern.com>
2021-07-18 22:32:11 -0400

    Debug

--- oauth.js
+++ oauth.js
@@ -1,11 +1,12 @@
 window.onload = () => {
   const fragment = new URLSearchParams(window.location.hash.slice(1));
   const accessToken = fragment.get('access_token');
-
-  const genAuth = new XMLHttpRequest();
-  genAuth.open("POST", "https://discordapp.com/api/oauth2/token?grant_type=identify&code="+accessToken);
-  genAuth.send();
-  genAuth.onload = function () {
-    console.log(genAuth.response);
-  }
+  
+  fetch("https://discordapp.com/api/oauth2/token?grant_type=identify&code=", {
+    method: 'post',
+    body: JSON.stringify(body),
+    headers: { 'grant_type': 'identify', 'code':accessToken },
+  })
+  .then(res => res.json())
+  .then(json => console.log(json))
 };
