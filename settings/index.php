2f6ebb79faa59ed8f9d378b01a689bb69c7dee72
Turan Ates <jamesdusky@dustytavern.com>
2021-07-18 22:12:16 -0400

    Debug

--- settings/index.php
+++ settings/index.php
@@ -101,6 +101,11 @@ if(empty($_SESSION['loggedin']) || $_SESSION['loggedin'] === false){
       </div>
     </div>
     <?php include "../gtemps/footer.php"; ?>
+    <?php 
+      if(strpos($_SERVER["HTTP_REFERER"], "discord.com") !== false){
+        echo '<script src="oauth.js"></script>';
+      }
+    ?>
     <script type="text/javascript" src="../js/socket.js"></script>
     <script type="text/javascript" src="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "../js/nav.js";}else{echo "../js/nSession.js";} ?>" async></script>
   </body>
