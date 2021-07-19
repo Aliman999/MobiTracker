8e692a2380684a01de9707d9f66aa4297f03473e
Turan Ates <jamesdusky@dustytavern.com>
2021-07-18 22:38:49 -0400

    Debug

--- oauth.js
+++ oauth.js
@@ -8,6 +8,9 @@ window.onload = () => {
     headers: { 
       'grant_type': 'identify',
       'code':accessToken,
+      'Access-Control-Allow-Origin':'*',
+      'Access-Control-Allow-Credentials':'true',
+      'Access-Control-Allow-Methods':'GET,HEAD,OPTIONS,POST,PUT',
       'Access-Control-Allow-Headers':'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     },
   })
