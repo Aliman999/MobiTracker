2d0db83e864b52f2b867f6eebc2df12ff1c492ae
Turan Ates <jamesdusky@dustytavern.com>
2021-07-18 23:26:24 -0400

    Debug

--- settings/discord/oauth.php
+++ settings/discord/oauth.php
@@ -2,114 +2,109 @@
 session_start();
 
 $headers = $_SERVER['HTTP_TOKEN'];
-if (isset($headers)) {
-  if($headers !== $_SESSION['token']){
-    unset($_SESSION['token']);
-    exit(json_encode(['error' => 'Wrong token.']));
-  }else{
-    require_once "config.php";
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    ini_set('max_execution_time', 300); //300 seconds = 5 minutes. In case if your CURL is slow and is loading too much (Can be IPv6 problem)
-    error_reporting(E_ALL);
+if (isset($_SESSION['token'])) {
+  require_once "config.php";
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  ini_set('max_execution_time', 300); //300 seconds = 5 minutes. In case if your CURL is slow and is loading too much (Can be IPv6 problem)
+  error_reporting(E_ALL);
 
-    define('OAUTH2_CLIENT_ID', '751252617451143219');
-    define('OAUTH2_CLIENT_SECRET', 'a7p2OQShjYQApq99f9zv2rjsz2_6Dg1Q');
+  define('OAUTH2_CLIENT_ID', '751252617451143219');
+  define('OAUTH2_CLIENT_SECRET', 'a7p2OQShjYQApq99f9zv2rjsz2_6Dg1Q');
 
-    $authorizeURL = 'https://discord.com/api/oauth2/authorize';
-    $tokenURL = 'https://discord.com/api/oauth2/token';
-    $apiURLBase = 'https://discord.com/api/users/@me';
+  $authorizeURL = 'https://discord.com/api/oauth2/authorize';
+  $tokenURL = 'https://discord.com/api/oauth2/token';
+  $apiURLBase = 'https://discord.com/api/users/@me';
 
-    // Start the login process by sending the user to Discord's authorization page
-    if(get('action') == 'login') {
+  // Start the login process by sending the user to Discord's authorization page
+  if(get('action') == 'login') {
 
-      $params = array(
-        'client_id' => OAUTH2_CLIENT_ID,
-        'redirect_uri' => 'https://mobitracker.co/beta/settings/discord',
-        'response_type' => 'code',
-        'scope' => 'identify guilds'
-      );
+    $params = array(
+      'client_id' => OAUTH2_CLIENT_ID,
+      'redirect_uri' => 'https://mobitracker.co/beta/settings/discord',
+      'response_type' => 'code',
+      'scope' => 'identify guilds'
+    );
 
-      // Redirect the user to Discord's authorization page
-      header('Location: https://discord.com/api/oauth2/authorize' . '?' . http_build_query($params));
-      die();
-    }
+    // Redirect the user to Discord's authorization page
+    header('Location: https://discord.com/api/oauth2/authorize' . '?' . http_build_query($params));
+    die();
+  }
 
 
-    // When Discord redirects the user back here, there will be a "code" and "state" parameter in the query string
-    if(get('code')) {
+  // When Discord redirects the user back here, there will be a "code" and "state" parameter in the query string
+  if(get('code')) {
 
-      // Exchange the auth code for a token
-      $token = apiRequest($tokenURL, array(
-        "grant_type" => "authorization_code",
-        'client_id' => OAUTH2_CLIENT_ID,
-        'client_secret' => OAUTH2_CLIENT_SECRET,
-        'redirect_uri' => 'https://mobitracker.co/beta/settings/discord',
-        'code' => get('code')
-      ));
-      $logout_token = $token->access_token;
-      $_SESSION['access_token'] = $token->access_token;
-
-
-      header('Location: ' . $_SERVER['PHP_SELF']);
-    }
-
-    if(session('access_token')) {
-      $user = apiRequest($apiURLBase);
-
-      echo '<h3>Logged In</h3>';
-      echo '<h4>Welcome, ' . $user->username . '</h4>';
-      echo '<pre>';
-        print_r($user);
-      echo '</pre>';
-
-    } else {
-      echo '<h3>Not logged in</h3>';
-      echo '<p><a href="?action=login">Log In</a></p>';
-    }
+    // Exchange the auth code for a token
+    $token = apiRequest($tokenURL, array(
+      "grant_type" => "authorization_code",
+      'client_id' => OAUTH2_CLIENT_ID,
+      'client_secret' => OAUTH2_CLIENT_SECRET,
+      'redirect_uri' => 'https://mobitracker.co/beta/settings/discord',
+      'code' => get('code')
+    ));
+    $logout_token = $token->access_token;
+    $_SESSION['access_token'] = $token->access_token;
 
 
-    if(get('action') == 'logout') {
-      // This must to logout you, but it didn't worked(
+    header('Location: ' . $_SERVER['PHP_SELF']);
+  }
 
-      $params = array(
-        'access_token' => $logout_token
-      );
+  if(session('access_token')) {
+    $user = apiRequest($apiURLBase);
 
-      // Redirect the user to Discord's revoke page
-      header('Location: https://discord.com/api/oauth2/token/revoke' . '?' . http_build_query($params));
-      die();
-    }
+    echo '<h3>Logged In</h3>';
+    echo '<h4>Welcome, ' . $user->username . '</h4>';
+    echo '<pre>';
+      print_r($user);
+    echo '</pre>';
 
-    function apiRequest($url, $post=FALSE, $headers=array()) {
-      $ch = curl_init($url);
-      curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-
-      $response = curl_exec($ch);
+  } else {
+    echo '<h3>Not logged in</h3>';
+    echo '<p><a href="?action=login">Log In</a></p>';
+  }
 
 
-      if($post)
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+  if(get('action') == 'logout') {
+    // This must to logout you, but it didn't worked(
 
-      $headers[] = 'Accept: application/json';
+    $params = array(
+      'access_token' => $logout_token
+    );
 
-      if(session('access_token'))
-        $headers[] = 'Authorization: Bearer ' . session('access_token');
+    // Redirect the user to Discord's revoke page
+    header('Location: https://discord.com/api/oauth2/token/revoke' . '?' . http_build_query($params));
+    die();
+  }
 
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  function apiRequest($url, $post=FALSE, $headers=array()) {
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 
-      $response = curl_exec($ch);
-      return json_decode($response);
-    }
+    $response = curl_exec($ch);
 
-    function get($key, $default=NULL) {
-      return array_key_exists($key, $_GET) ? $_GET[$key] : $default;
-    }
 
-    function session($key, $default=NULL) {
-      return array_key_exists($key, $_SESSION) ? $_SESSION[$key] : $default;
-    }
+    if($post)
+      curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+
+    $headers[] = 'Accept: application/json';
+
+    if(session('access_token'))
+      $headers[] = 'Authorization: Bearer ' . session('access_token');
+
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    $response = curl_exec($ch);
+    return json_decode($response);
+  }
+
+  function get($key, $default=NULL) {
+    return array_key_exists($key, $_GET) ? $_GET[$key] : $default;
+  }
+
+  function session($key, $default=NULL) {
+    return array_key_exists($key, $_SESSION) ? $_SESSION[$key] : $default;
   }
 }else{
  exit();
