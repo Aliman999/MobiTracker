e2cab98b2e703ac757b9e880ebd459f42fb6a2d3
Turan Ates <jamesdusky@dustytavern.com>
2021-07-24 18:49:01 -0400

    Debug

---
+++ admin/api.js
@@ -0,0 +1,149 @@
+var tokenHeader = document.getElementsByName("token")[0];
+var jwt = document.getElementsByName("jwt")[0];
+var webSocket = null;
+var bool = true;
+
+function getColor() {
+  var hue = Math.floor(Math.random() * 360);
+  return 'hsl(' + hue + ', 100%, 80%)';
+}
+
+if(jwt){
+  socket();
+}else{
+  var loading = document.getElementById("loadingContainer")
+  loading.style.opacity = 0;
+  setTimeout(() => {
+    loading.remove();
+  }, 250);
+}
+
+var bars = [
+  new ldBar(".players", {
+    "preset": 'circle',
+    "stroke": getColor(),
+    "img": '',
+    "fill-dir": 'btt',
+    "fill": '#25b',
+    "fill-background": '#fff',
+    "fill-background-extrude": 3,
+    "pattern-size": 3,
+    "stroke-dir": 'normal',
+    "stroke-width": '6',
+    "stroke-trail": '#ddd',
+    "stroke-trail-width": 0.5,
+    "duration": 1,
+    "easing": 'linear',
+    "value": 0,
+    "img-size": null,
+    "set-dim": true,
+    "aspect-ratio": "xMidYMid",
+    "transition-in": false,
+    "min": 0,
+    "max": 100,
+    "precision": 0,
+    "padding": undefined
+  }),
+  new ldBar(".crawler", {
+    "preset": 'circle',
+    "stroke": getColor(),
+    "img": '',
+    "fill-dir": 'btt',
+    "fill": '#25b',
+    "fill-background": '#ddd',
+    "fill-background-extrude": 3,
+    "pattern-size": null,
+    "stroke-dir": 'normal',
+    "stroke-width": '6',
+    "stroke-trail": '#ddd',
+    "stroke-trail-width": 0.5,
+    "duration": 1,
+    "easing": 'linear',
+    "value": 0,
+    "img-size": null,
+    "set-dim": true,
+    "aspect-ratio": "xMidYMid",
+    "transition-in": false,
+    "min": 0,
+    "max": 100,
+    "precision": 0,
+    "padding": undefined
+  }),
+  new ldBar(".scanner", {
+    "preset": 'circle',
+    "stroke": getColor(),
+    "img": '',
+    "fill-dir": 'btt',
+    "fill": '#25b',
+    "fill-background": '#ddd',
+    "fill-background-extrude": 3,
+    "pattern-size": null,
+    "stroke-dir": 'normal',
+    "stroke-width": '6',
+    "stroke-trail": '#ddd',
+    "stroke-trail-width": 0.5,
+    "duration": 1,
+    "easing": 'linear',
+    "value": 0,
+    "img-size": null,
+    "set-dim": true,
+    "aspect-ratio": "xMidYMid",
+    "transition-in": false,
+    "min": 0,
+    "max": 100,
+    "precision": 0,
+    "padding": undefined
+  })
+];
+
+function display(response){
+  bars[0].set(Math.floor((response.data.player.current / response.data.player.max * 100)));
+  bars[1].set(Math.floor((response.data.crawler.current / response.data.crawler.max * 100)));
+  bars[2].set(Math.floor((response.data.scanner.current / response.data.scanner.max * 100)));
+}
+
+function socket(){
+  var connection = document.getElementsByClassName("conContainer")[0];
+  webSocket = new WebSocket("wss://ws.mobitracker.co:2599");
+  webSocket.onopen = function(){
+    connection.children[0].innerText = "CONNECTING";
+    connection.children[1].className = "connecting";
+    message = {
+      type:"panel",
+      token:jwt.content
+    };
+    webSocket.send(JSON.stringify(message));
+    heartbeat();
+  }
+  webSocket.onmessage = function(event){
+    var response = JSON.parse(event.data);
+    if(response.type == "authentication"){
+      var loading;
+      if (loading = document.getElementById("loadingContainer")){
+        loading.style.opacity = 0;
+        setTimeout(() => {
+          loading.remove();
+        }, 250);
+      }
+    } else {
+      connection.children[0].innerText = "LIVE";
+      connection.children[1].className = "live";
+      display(response);
+      bool = false;
+    }
+  }
+
+  webSocket.onclose = function () {
+    connection.children[0].innerText = "DISCONNECTED";
+    connection.children[1].className = "offline";
+    console.log("Connection Closed");
+    setTimeout(socket, 3000);
+  };
+}
+
+function heartbeat() {
+  if (!webSocket) return;
+  if (webSocket.readyState !== 1) return;
+  webSocket.send(JSON.stringify({type:"ping"}));
+  setTimeout(heartbeat, 3000);
+}
---
+++ admin/index.php
@@ -0,0 +1,114 @@
+<?php
+// Initialize the session
+session_start();
+if (empty($_SESSION['token'])) {
+  $_SESSION['token'] = bin2hex(random_bytes(32));
+}
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>MobiTracker</title>
+    <link href="https://fonts.googleapis.com/css2?family=Exo:wght@300;400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css?v=2.0">
+    <link rel="stylesheet" href="../css/patreon.css">
+    <link rel="stylesheet" href="../css/nav.css">
+    <link rel="stylesheet" href="locale.css">
+
+    <link rel="stylesheet" href="mk_charts.css" />
+    <link rel="stylesheet" href="loading-bar.css" />
+
+    <link rel="apple-touch-icon" sizes="180x180" href="https://mobitracker.co/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://mobitracker.co/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://mobitracker.co/favicon-16x16.png">
+    <link rel="manifest" href="https://mobitracker.co/site.webmanifest">
+    <link rel="mask-icon" href="https://mobitracker.co/safari-pinned-tab.svg" color="#5bbad5">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="MobiTracker.co" />
+    <meta property="og:description" content="Find who to trust when you need a hand to lead your armed escort or who to avoid letting on your ship" />
+    <meta property="og:url" content="https://mobitracker.co" />
+    <meta property="og:image" content="https://mobitracker.co/android-chrome-512x512.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#253139">
+    <?php
+    echo "<meta name='token' content=".$_SESSION['token'].">";
+    ?>
+    <?php
+    if($_SESSION["loggedin"]){
+      echo "<meta name='jwt' content=".$_SESSION['jwt'].">";
+    }
+    ?>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-177343701-1"></script>
+    <script type="text/javascript" src="../js/ga.js"></script>
+  </head>
+
+  <body>
+    <?php include "../gtemps/nav.php"; ?>
+    <div id="canvas">
+      <div class="headerContainer">
+        <div id="login_status" class="login_menu">
+          <?php include "../gtemps/titleBar.php" ?>
+          <div id="navBtnContainer" class="navBtnContainer">
+            <div class="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "loggedIn";}else{echo "loggedOut";} ?>"></div>
+            <div class="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "loggedIn";}else{echo "loggedOut";} ?>"></div>
+            <div class="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "loggedIn";}else{echo "loggedOut";} ?>"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="titleContainer">
+        <p class="webTitle">Systems</p>
+      </div>
+      <div class="container-header">
+        <div class="centerCont">
+          <div id="loadingContainer">
+            <p><span class="rBold">Loading</span></p><img src="../../src/loading.png" class="loading">
+          </div>
+          <div class="conContainer">
+            <p>DISCONNECTED</p><div class="offline"></div>
+          </div>
+          <div>
+            <div class="players label-center"></div>
+            <p>Player Scanner</p>
+          </div>
+          <div>
+            <div class="crawler label-center"></div>
+            <p>Org Crawler</p>
+          </div>
+          <div>
+            <div class="scanner label-center"></div>
+            <p>Org Scanner</p>
+          </div>
+        </div>
+      </div>
+      <div class="container-header">
+        <div class="centerCont">
+          <div id="loadingContainer">
+            <p><span class="rBold">Loading</span></p><img src="../../src/loading.png" class="loading">
+          </div>
+          <div class="conContainer">
+            <p>DISCONNECTED</p><div class="offline"></div>
+          </div>
+          <div>
+            <div class="players label-center"></div>
+            <p>Player Scanner</p>
+          </div>
+          <div>
+            <div class="crawler label-center"></div>
+            <p>Org Crawler</p>
+          </div>
+          <div>
+            <div class="scanner label-center"></div>
+            <p>Org Scanner</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <?php include "../gtemps/footer.php"; ?>
+    <script src="loading-bar.js"></script>
+    <script type="text/javascript" src="api.js"></script>
+    <script type="text/javascript" src="<?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){echo "../js/nav.js";}else{echo "../js/nSession.js";} ?>" async></script>
+  </body>
+</html>
