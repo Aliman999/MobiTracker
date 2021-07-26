ec2f22d6f459723331ee1a4c27677e79f67c2e92
Turan Ates <jamesdusky@dustytavern.com>
2021-07-26 00:19:22 -0400

    Debug

--- settings/history/main.js
+++ settings/history/main.js
@@ -169,7 +169,7 @@ function init(){
 
   var field = document.getElementsByClassName("setting")[0];
   var line = timeline.container();
-  var elem = timeline.elem("First Entry", "Welcome JamesDusky into the System", 19, "Jul", "01/12/05", "12:30", [{ href: "#", text: "Hello World" }]);
+  var elem = timeline.elem("First Entry", "Welcome JamesDusky into the System", 19, "Jul", "01/12/05", "12:30", 0, [{ href: "#", text: "Hello World" }]);
   timeline.left(elem);
   line.appendChild(elem);
   field.appendChild(line);
