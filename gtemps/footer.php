0d2eec46db0473c63540024ef89ccc475d68bf82
Turan Ates <jamesdusky@dustytavern.com>
2021-08-02 11:06:42 -0400

    new cp

--- gtemps/footer.php
+++ gtemps/footer.php
@@ -3,7 +3,7 @@
   <div class="fContainer">
     <div class="fCopy">
       <h3>MobiTracker</h3>
-      <p>Copyright © 2019 - 2021 J. Dusky</p>
+      <p>Copyright © 2019 - 2021 MobiTracker. All rights reserved.</p>
       <span>Star Citizen®, Roberts Space Industries® and Cloud Imperium® are registered trademarks of Cloud Imperium Rights LLC</span><span>We are not affiliated or associated with Roberts Space Industries or Cloud Imperium Games.</span>
     </div>
     <div class="fLinks">
