ca29a91bc1c9aca87589a34e25313d68d86e381b
Turan Ates <jamesdusky@dustytavern.com>
2021-08-07 12:42:03 -0400

    Debug

--- settings/history/main.js
+++ settings/history/main.js
@@ -160,7 +160,7 @@ function init(){
           action.innerText = actions[x].text;
 
           action.onclick = function(){
-            desc.innerHTML = extra;
+            desc.innerText = JSON.parse(extra);
           }
 
           eventActions.appendChild(action);
