8e4da48aee12fee267ae4c99996b812e6c6c3fdb
Turan Ates <jamesdusky@dustytavern.com>
2021-08-10 19:02:52 -0400

    Debug

--- email/update.php
+++ email/update.php
@@ -19,6 +19,7 @@ $ciphertext = $_GET['token'];
 $cipher = "aes-128-gcm";
 $key = "Ke7CF6gytaMufbSL-cwEFA";
 $tag = "gcm";
+$decrypted = null;
 if (in_array($cipher, openssl_get_cipher_methods())){
   $ivlen = openssl_cipher_iv_length($cipher);
   $iv = openssl_random_pseudo_bytes($ivlen);
