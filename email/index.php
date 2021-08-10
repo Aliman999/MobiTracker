ff48d29c465e972dad0fd5c6bcf864aa0b8cf9c1
Turan Ates <jamesdusky@dustytavern.com>
2021-08-10 18:24:41 -0400

    Debug

--- email/index.php
+++ email/index.php
@@ -1,8 +1,7 @@
 <?php
 if(!empty($_GET['token'])){
-  var_dump($_GET);
-  die("503 Forbidden. Turn around and make a left turn."); 
-}else{
   define('include', TRUE);
   include "update.php";
+}else{
+  die("503 Forbidden. Turn around and make a left turn."); 
 }
