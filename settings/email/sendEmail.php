950d3695c340248f7aff43b1f20eda8776ab2c29
Turan Ates <jamesdusky@dustytavern.com>
2021-08-10 20:21:31 -0400

    Debug

--- settings/email/sendEmail.php
+++ settings/email/sendEmail.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 session_start();
 
 ini_set('display_errors', 1);
@@ -17,7 +18,6 @@ if(isset($headers)){
     exit(json_encode(['error' => 'Wrong token.']));
   }else{
     if(!empty($_POST['email'])){
-      declare(strict_types=1);
       /**
        * Encrypt a message
        * 
