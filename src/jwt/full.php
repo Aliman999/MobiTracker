7b75b33f31effdda96d6e6c109602194e32c74f6
Turan Ates <jamesdusky@dustytavern.com>
2021-09-04 00:48:51 -0400

    added jwt for quick and full

---
+++ src/jwt/full.php
@@ -0,0 +1,37 @@
+<?php
+require 'bootstrap.php';
+
+use Carbon\Carbon;
+
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
+
+$secret = $_ENV['FULL'];
+
+$header = json_encode([
+    'typ' => 'JWT',
+    'alg' => 'HS256'
+]);
+
+$payload = json_encode([
+    'cid' => $_SESSION['cID'],
+    'username' => $_SESSION['username'],
+    'privilage' => 0, //Disabled
+    'prio' => $_SESSION['prio'],
+    'type' => "player",
+    'query' => $_GET['search'],
+    'iat' => time() + (30 * 60)
+]);
+
+$base64UrlHeader = base64UrlEncode($header);
+
+$base64UrlPayload = base64UrlEncode($payload);
+
+$signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, $secret, true);
+
+$base64UrlSignature = base64UrlEncode($signature);
+
+$jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
+
+$_SESSION['fullToken'] = $jwt;
