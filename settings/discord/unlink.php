d973af8852ee6f60b55c957bbddf2872e4a040b1
Turan Ates <jamesdusky@dustytavern.com>
2021-09-05 20:59:05 -0400

    Debug

--- settings/discord/unlink.php
+++ settings/discord/unlink.php
@@ -8,7 +8,8 @@ if(isset($headers)){
     unset($_SESSION['token']);
     exit(json_encode(['error' => 'Wrong token.']));
   }else{
-    $sql = "UPDATE discord SET (`username`, `cID`) VALUES (null, null) WHERE discUser LIKE '%".$_SESSION["username"]."%';";
+    $sql = "SELECT username, cID FROM discord WHERE username LIKE '%" . $_SESSION["username"] . "%';"
+    //$sql = "UPDATE discord SET (`username`, `cID`) VALUES (null, null) WHERE discUser LIKE '%".$_SESSION["username"]."%';";
     echo $sql;
     //mysqli_query($link, $sql);
   }
