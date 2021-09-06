787ee0e567731cd42ffb1684e7b46afaccfa5520
Turan Ates <jamesdusky@dustytavern.com>
2021-09-06 19:12:35 -0400

    Debug

--- js/nSession.js
+++ js/nSession.js
@@ -6,8 +6,12 @@ var eleCount = document.getElementsByClassName("navEle");
 var navMenu = document.getElementById("navMenu");
 var query = new XMLHttpRequest();
 openBtn.active = false;
+if(!navRegister){
+  var navRegister = false;
+}
 
 var x = 0;
+
 var urTimer = setInterval(()=>{
   if (navRegister && x <= 3) {
     clearInterval(urTimer);
