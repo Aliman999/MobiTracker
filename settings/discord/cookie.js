6cca4dc40e9d6dd942245243f9e68318bd5d691b
Turan Ates <jamesdusky@dustytavern.com>
2021-09-06 00:26:58 -0400

    Debug

--- settings/discord/cookie.js
+++
@@ -1,4 +0,0 @@
-var username = document.getElementsByName("username")[0];
-var id = document.getElementsByName("id")[0];
-document.cookie = "username="+username.content+";";
-document.cookie = "cid=" + id.content + ";";
--- settings/discord/oauth.php
+++ settings/discord/oauth.php
@@ -97,7 +97,7 @@ function get($key, $default=NULL) {
   return array_key_exists($key, $_GET) ? $_GET[$key] : $default;
 }
 
-function session($key, $default=NULL) {
-  return array_key_exists($key, $_SESSION) ? $_SESSION[$key] : $default;
+function session($key) {
+  return array_key_exists($key, $_SESSION) ? $_SESSION[$key] : $_SESSION[$key];
 }
 ?>
