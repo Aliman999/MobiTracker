f6bde1ec831d146c918b3a4353c4041d3f54ebef
Turan Ates <jamesdusky@dustytavern.com>
2021-09-08 11:50:37 -0400

    Debug

--- organization/index.php
+++ organization/index.php
@@ -16,10 +16,10 @@
     <title>Reputation Hub - MobiTracker </title>
     <link href="https://fonts.googleapis.com/css2?family=Exo:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css?v=2.0">
-    <link rel="stylesheet" href="css/patreon.css">
-    <link rel="stylesheet" href="css/nav.css">
-    <link rel="stylesheet" href="css/stars.css">
+    <link rel="stylesheet" href="../css/style.css?v=2.0">
+    <link rel="stylesheet" href="../css/patreon.css">
+    <link rel="stylesheet" href="../css/nav.css">
+    <link rel="stylesheet" href="../css/stars.css">
     <link rel="apple-touch-icon" sizes="180x180" href="https://mobitracker.co/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="https://mobitracker.co/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="https://mobitracker.co/favicon-16x16.png">
