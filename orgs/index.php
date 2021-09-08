7ff616c11a6e1df97d2e6bbd1b35f106840a97db
Turan Ates <jamesdusky@dustytavern.com>
2021-09-08 13:23:32 -0400

    Debug

--- orgs/index.php
+++ orgs/index.php
@@ -70,7 +70,7 @@
         <p class="webTitle">Reputation Hub</p>
       </div>
       <div class="inputContainer padded" id="iContainer">
-        <input type="text" class="userInput" autofocus value="<?php echo $_SESSION['search']; ?>" placeholder="Enter a Star Citizen's Handle">
+        <input type="text" class="userInput" autofocus value="<?php echo $_SESSION['search']; ?>" placeholder="Enter an Organization's SID">
       </div>
       <div class="search-param-container padded" id="sPContainer">
         Search By:
